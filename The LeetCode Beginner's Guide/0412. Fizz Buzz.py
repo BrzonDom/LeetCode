2c585afdbc3fb1b6f810bbafb6218a9da6db07d6
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-02-08 13:42:15 +0100

    Beg. Guide 0412 v1.0

---
+++ The LeetCode Beginner's Guide/0412. Fizz Buzz.py
@@ -0,0 +1,60 @@
+"""
+412. Fizz Buzz
+
+https://leetcode.com/problems/fizz-buzz/description/
+
+    Given an integer n, return a string array answer (1-indexed) where:
+
+    answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+    answer[i] == "Fizz" if i is divisible by 3.
+    answer[i] == "Buzz" if i is divisible by 5.
+    answer[i] == i (as a string) if none of the above conditions are true.
+
+    Example 1:
+
+        Input: n = 3
+        Output: ["1","2","Fizz"]
+
+    Example 2:
+
+        Input: n = 5
+        Output: ["1","2","Fizz","4","Buzz"]
+
+    Example 3:
+
+        Input: n = 15
+        Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+
+    Constraints:
+
+        1 <= n <= 104
+
+"""
+
+Input_num = [3, 5, 15]
+
+print("Driver solution:\n")
+
+for num in Input_num:
+
+    answerOut = []
+    Fizz = "Fizz"
+    Buzz = "Buzz"
+
+    print(f"\tFor range {num}")
+
+    for n in range(1, num+1):
+        print(f"\t\t {n:2}", end=".\t")
+
+        if not (n % 3 and n % 5):
+            print(Fizz+Buzz)
+        elif not n % 3:
+            print(Fizz)
+        elif not n % 5:
+            print(Buzz)
+        else:
+            print(str(n))
+
+    print()
+
