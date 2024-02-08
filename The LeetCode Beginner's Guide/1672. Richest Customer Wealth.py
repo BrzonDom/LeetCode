d168eef3c48597ba9834f90ce6700110ad8a11a4
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-02-08 13:28:46 +0100

    Beg. Guide 1672 v2.2

--- The LeetCode Beginner's Guide/1672. Richest Customer Wealth.py
+++ The LeetCode Beginner's Guide/1672. Richest Customer Wealth.py
@@ -1,35 +1,37 @@
 """
+1672. Richest Customer Wealth
+
 https://leetcode.com/problems/richest-customer-wealth
 
-You are given an m x n integer grid accounts where
-accounts[i][j] is the amount of money the i^th customer has in the j^th bank.
+    You are given an m x n integer grid accounts where
+    accounts[i][j] is the amount of money the i^th customer has in the j^th bank.
 
-Return the wealth that the richest customer has.
+    Return the wealth that the richest customer has.
 
-A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+    A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
 
-Example 1:
+    Example 1:
 
-    Input: accounts = [[1,2,3],[3,2,1]]
-    Output: 6
+        Input: accounts = [[1,2,3],[3,2,1]]
+        Output: 6
 
-Example 2:
+    Example 2:
 
-    Input: accounts = [[1,5],[7,3],[3,5]]
-    Output: 10
+        Input: accounts = [[1,5],[7,3],[3,5]]
+        Output: 10
 
-Example 3:
+    Example 3:
 
-    Input: accounts = [[2,8,7],[7,1,3],[1,9,5]]
-    Output: 17
+        Input: accounts = [[2,8,7],[7,1,3],[1,9,5]]
+        Output: 17
 
 
-Constraints:
+    Constraints:
 
-    m == accounts.length
-    n == accounts[i].length
-    1 <= m, n <= 50
-    1 <= accounts[i][j] <= 100
+        m == accounts.length
+        n == accounts[i].length
+        1 <= m, n <= 50
+        1 <= accounts[i][j] <= 100
 
 """
 
