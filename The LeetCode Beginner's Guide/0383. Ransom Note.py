302bdfa378437f4ecf46dcac28d044d356414adb
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-02-08 18:32:17 +0100

    Beg. Guide 0383 v1.0

---
+++ The LeetCode Beginner's Guide/0383. Ransom Note.py
@@ -0,0 +1,74 @@
+"""
+383. Ransom Note
+
+https://leetcode.com/problems/ransom-note/description/
+
+    Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+    Each letter in magazine can only be used once in ransomNote.
+
+    Example 1:
+
+        Input: ransomNote = "a", magazine = "b"
+        Output: false
+
+    Example 2:
+
+        Input: ransomNote = "aa", magazine = "ab"
+        Output: false
+
+    Example 3:
+
+        Input: ransomNote = "aa", magazine = "aab"
+        Output: true
+
+    Constraints:
+
+        1 <= ransomNote.length, magazine.length <= 105
+        ransomNote and magazine consist of lowercase English letters.
+
+"""
+
+Input_charLst = [["a", "b"],
+                 ["aa", "ab"],
+                 ["aa", "aab"]]
+
+
+print("Test code:\n")
+
+Note = "aabcdaehe"
+Magaz = "abbcadeeaedfea"
+
+print(f"\tNote:      {Note}")
+print(f"\tMagazine:  {Magaz}")
+print()
+
+Note_dic = {}
+Magz_dic = {}
+
+MissChar_Lst = []
+
+
+for char in Note:
+    if char not in Note_dic:
+        Note_dic[char] = Note.count(char)
+        Magz_dic[char] = Magaz.count(char)
+
+        if Note.count(char) < Magaz.count(char):
+            MissChar_Lst.append(char)
+
+
+print(f"\tNote dictionary:   {Note_dic}")
+print(f"\tMagaz dictionary:  {Magz_dic}")
+
+for key in Note_dic:
+    print(f"\t\t{key} : {Note_dic[key]} / {Magz_dic[key]}")
+    if not Magz_dic[key]:
+        print(f"\t\t\tMissing {key} letter !")
+print()
+
+
+
+
+
+
