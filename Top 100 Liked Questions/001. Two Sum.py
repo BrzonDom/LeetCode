b0287d8960493945926daf4e7d5734319eda2ffb
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-02-09 05:39:50 +0100

    100. Liked 001 v3.2

--- Top 100 Liked Questions/001. Two Sum.py
+++ Top 100 Liked Questions/001. Two Sum.py
@@ -50,39 +50,38 @@ for numLst in Input_numsLst:
 
     print()
 
+# print("1.Driver solution:\n")
+#
+# for numLst in Input_numsLst:
+#     numArr = numLst[0]
+#     targ = numLst[1]
+#
+#     print("\tNumbers array:", numArr)
+#     print("\t       Target:", targ)
+#     print()
+#
+#     combFound = False
+#     combTarg = []
+#
+#     for n1, num1 in enumerate(numArr):
+#         if combFound:
+#             break
+#
+#         for n2, num2 in enumerate(numArr):
+#             if n1 != n2:
+#                 print(f"\t\t\t\tnums[{n1}] + nums[{n2}] = {num1:2} + {num2:2} = {num1 + num2:2}")
+#
+#                 if num1 + num2 == targ:
+#                     print("\n\t\tFound combination:")
+#                     print(f"\t\t\tnums[{n1}] + nums[{n2}] = {num1:2} + {num2:2} = {num1 + num2:2}")
+#
+#                     combTarg.append([n1, num1])
+#                     combTarg.append([n2, num2])
+#                     combFound = True
+#                     break
+#     print("\n")
+
 print("1.Driver solution:\n")
-
-for numLst in Input_numsLst:
-    numArr = numLst[0]
-    targ = numLst[1]
-
-    print("\tNumbers array:", numArr)
-    print("\t       Target:", targ)
-    print()
-
-    combFound = False
-    combTarg = []
-
-    for n1, num1 in enumerate(numArr):
-        if combFound:
-            break
-
-        for n2, num2 in enumerate(numArr):
-            if n1 != n2:
-                print(f"\t\t\t\tnums[{n1}] + nums[{n2}] = {num1:2} + {num2:2} = {num1 + num2:2}")
-
-                if num1 + num2 == targ:
-                    print("\n\t\tFound combination:")
-                    print(f"\t\t\tnums[{n1}] + nums[{n2}] = {num1:2} + {num2:2} = {num1 + num2:2}")
-
-                    combTarg.append([n1, num1])
-                    combTarg.append([n2, num2])
-                    combFound = True
-                    break
-
-    print("\n")
-
-print("2.Driver solution:\n")
 
 for numLst in Input_numsLst:
     numArr = numLst[0]
@@ -109,3 +108,32 @@ for numLst in Input_numsLst:
                 break
         print()
     print()
+
+print("2.Driver solution:\n")
+
+for numLst in Input_numsLst:
+    numArr = numLst[0]
+    targ = numLst[1]
+
+    print("\tNumbers array:", numArr)
+    print("\t       Target:", targ)
+    print()
+
+    combFound = False
+
+    for n1, num1 in enumerate(numArr):
+
+        remain = targ - num1
+        print(f"\t\t\t\tTarget - nums[{n1}] = {targ:2} - {num1:2} = {remain}")
+
+        if remain in numArr[n1+1:]:
+            num2 = remain
+            n2 = numArr.index(num2, n1+1)
+            print("\n\t\tFound combination:")
+            print(f"\t\t\tnums[{n1}] + nums[{n2}] = {num1:2} + {num2:2} = {num1 + num2:2}")
+            break
+    print("\n")
+
+
+
+
