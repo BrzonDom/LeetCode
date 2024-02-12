4e47fd7c787bc251eeb166f4ec03de14ebd3f68b
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-02-13 00:48:43 +0100

    100. Liked 005 v3.0

--- Top 100 Liked Questions/005. Longest Palindromic Substring.py
+++ Top 100 Liked Questions/005. Longest Palindromic Substring.py
@@ -22,11 +22,7 @@ https://leetcode.com/problems/longest-palindromic-substring/description/
         d consist of only digits and English letters.
 """
 
-
-def findPalStrV2_Prt(s):
-
-    print("\tString: ", s, "\n")
-
+def findPalStr_Sol(s):
     maxLen = 1
     maxPal = s[0]
     palFnd = True
@@ -40,22 +36,86 @@ def findPalStrV2_Prt(s):
 
         while True:
 
+            if l - 1 >= 0 and r + 1 < len(s) and s[l - 1] == s[r + 1]:
+                l -= 1
+                r += 1
+                curLen += 2
+                continue
+
+            if l == ind and r + 1 < len(s) and s[r + 1] == char:
+                r += 1
+                curLen += 1
+                continue
+
+            break
+
+        if curLen > maxLen:
+            maxLen = curLen
+            maxPal = s[l:r + 1]
+
+    return maxPal
+
+
+def findPalStrV2_Prt(s):
+    """
+        Idea:
+            Tries to build the longest palindrome one letter at a time
+            with continuously widening the sub-string
+    """
+
+
+    print("\tString: ", s, "\n")
+
+    maxLen = 1
+    maxPal = s[0]
+    palFnd = True
+
+    for ind, char in enumerate(s):
+        """     Every character is set as the center of the palindrome  """
+
+        l = ind
+        r = ind
+
+        curLen = 1
+
+        while True:
+
             if l-1 >= 0 and r+1 < len(s) and s[l-1] == s[r+1]:
+                """     For a case where both left and right indexes can be moved
+                            If both left-1 and right+1 indexes fit into the string
+                                and 
+                            if str[left-1] character == str[right+1] character
+                            the sub-string str[l-1:r+2] is a palindrome
+                """
 
                 print("\t\t", s[l-1:r+2])
+
                 l -= 1
                 r += 1
                 curLen += 2
                 continue
 
             if r+1 < len(s) and s[r+1] == char:
+                """     For a case where the left index can't be moved more
+                            If right+1 index fits into the string
+                                and
+                            if str[right+1] character == current character
+                            the sub-string str[l:r+2] is a palindrome 
+                """
 
                 print("\t\t", s[l:r+2])
+
                 r += 1
                 curLen += 1
                 continue
 
+            """     If neither of the previous cases are True
+                        we can move to the next character
+                        as the center of a palindrome
+            """
+
             break
+
 
         if curLen > maxLen:
             maxLen = curLen
