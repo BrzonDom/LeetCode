1bfed5e0292bfa9b9ff1cd7678b1e36482dc6aa8
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-06-07 01:59:39 +0200

    100. Liked 018 v01.01 Problem input

--- Top 100 Liked Questions/018. 4Sum.py
+++ Top 100 Liked Questions/018. 4Sum.py
@@ -5,7 +5,6 @@ https://leetcode.com/problems/4sum/description/
 
     Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c],
     nums[d]] such that:
-
         0 <= a, b, c, d < n
         a, b, c, and d are distinct.
         nums[a] + nums[b] + nums[c] + nums[d] == target
@@ -14,13 +13,13 @@ https://leetcode.com/problems/4sum/description/
 
     Example 1:
 
-        Input: nums = [1,0,-1,0,-2,2], target = 0
-        Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+        Input: nums = [1, 0, -1, 0, -2, 2], target = 0
+        Output: [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
 
     Example 2:
 
-        Input: nums = [2,2,2,2,2], target = 8
-        Output: [[2,2,2,2]]
+        Input: nums = [2, 2, 2, 2, 2], target = 8
+        Output: [[2, 2, 2, 2]]
 
     Constraints:
 
@@ -29,3 +28,16 @@ https://leetcode.com/problems/4sum/description/
         -10^9 <= target <= 10^9
 
 """
+
+Input_List = [[[1, 0, -1, 0, -2, 2], 0],
+              [[2, 2, 2, 2, 2], 8]]
+
+for InCase in Input_List:
+
+    numLst = InCase[0]
+    trgt = InCase[1]
+
+    print(f"\tNumbers: {numLst}")
+    print(f"\tTarger: {trgt}")
+
+    print()
