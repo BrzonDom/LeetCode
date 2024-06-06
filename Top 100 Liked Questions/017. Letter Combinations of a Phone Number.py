15b3986af2d71b117329732d9b2a4d1060d4cf12
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-06-06 19:22:19 +0200

    100. Liked 017 v02.01 Driver solution progress

--- Top 100 Liked Questions/017. Letter Combinations of a Phone Number.py
+++ Top 100 Liked Questions/017. Letter Combinations of a Phone Number.py
@@ -30,18 +30,19 @@ https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
         digits[i] is a digit in the range ['2', '9'].
 
 """
+import itertools
 
-Input_numComb=["23", "", "2"]
 
-phoneConv = {"1": "_",
-             "2": "abc",
-             "3": "def",
-             "4": "ghi",
-             "5": "jkl",
-             "6": "mno",
-             "7": "pqrs",
-             "8": "tuv",
-             "9": "wxyz"}
+Input_numComb= ["23", "", "2"]
+
+phoneConv = {"2": ["a", "b", "c"],
+             "3": ["d", "e", "f"],
+             "4": ["g", "h", "i"],
+             "5": ["j", "k", "l"],
+             "6": ["m", "n", "o"],
+             "7": ["p", "q", "r", "s"],
+             "8": ["t", "u", "v"],
+             "9": ["w", "x", "y", "z"]}
 
 
 for numCnt, numComb in enumerate(Input_numComb):
@@ -50,7 +51,11 @@ for numCnt, numComb in enumerate(Input_numComb):
 
     print(f"\t\"{numComb}\"\n")
 
+    # lenComb = len(numComb)
+
     for dig in numComb:
         print(f"\t{dig}: {phoneConv[dig]}")
+
+    # letComb = list(product())
 
     print("\n")
