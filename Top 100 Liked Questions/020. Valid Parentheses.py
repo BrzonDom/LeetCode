d977c48baf675570d277377c112463bec8be7227
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-06-10 20:31:04 +0200

    100. Liked 020 v05.03 2.Function progress

--- Top 100 Liked Questions/020. Valid Parentheses.py
+++ Top 100 Liked Questions/020. Valid Parentheses.py
@@ -253,6 +253,73 @@ def Sol02A_IterQue_Prt(inLst):
         print("\n")
 
 
+def Sol02_IterQue_Prt(prnths):
+
+    # for csCnt, prnths in enumerate(inLst):
+    #     print(f"{csCnt + 1}. Case")
+    #     print()
+
+    print(f"\tParentheses: {prnths}")
+    print()
+
+    stckPrnth = []
+    wrngPrnth = False
+    lessPrnth = False
+
+    for par in prnths:
+
+        if par == '(':
+            stckPrnth.append(')')
+
+            print(f"\t\t\"{par}\"\tLeft parentheses")
+            print("\t\t\t", stckPrnth)
+
+        elif par == '[':
+            stckPrnth.append(']')
+
+            print(f"\t\t\"{par}\"\tLeft parentheses")
+            print("\t\t\t", stckPrnth)
+
+        elif par == '{':
+            stckPrnth.append('}')
+
+            print(f"\t\t\"{par}\"\tLeft parentheses")
+            print("\t\t\t", stckPrnth)
+
+        elif not stckPrnth:
+            print(f"\t\t\"{par}\"\tRight parentheses")
+            print("\t\t\t", stckPrnth)
+
+            lessPrnth = True
+            break
+
+        elif stckPrnth.pop() != par:
+            print(f"\t\t\"{par}\"\tRight parentheses")
+            print("\t\t\t", stckPrnth)
+
+            wrngPrnth = True
+            break
+
+        else:
+            print(f"\t\t\"{par}\"\tRight parentheses")
+            print("\t\t\t", stckPrnth)
+
+        print()
+
+    print()
+
+    if wrngPrnth:
+        print("\tInvalid - Wrong parentheses")
+
+    elif stckPrnth or lessPrnth:
+        print("\tInvalid - Not enough parentheses")
+
+    else:
+        print("\tValid")
+
+    print("\n")
+
+
 if __name__ == '__main__':
 
     InputLst = ["()",
