0b64b97527ca6db2277bc47a6be06f94201bf34b
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-06-11 10:46:48 +0200

    100. Liked 021 v02.01 Tools progress

--- Top 100 Liked Questions/021. Merge Two Sorted Lists.py
+++ Top 100 Liked Questions/021. Merge Two Sorted Lists.py
@@ -34,6 +34,12 @@ https://leetcode.com/problems/merge-two-sorted-lists/description/
 """
 
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 InputLst = [[[1, 2, 4], [1, 3,  4]],
             [[], []],
             [[], [0]]]
