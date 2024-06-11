c722fabe684685925f53fcb4d6ca495b91d2b729
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-06-11 12:29:34 +0200

    100. Liked 021 v02.08 Driver progress

--- Top 100 Liked Questions/021. Merge Two Sorted Lists.py
+++ Top 100 Liked Questions/021. Merge Two Sorted Lists.py
@@ -59,7 +59,8 @@ class LinkList:
 
 
 InputLst = [[[1, 2, 4], [1, 3,  4]],
-            [[], []],
+            [[1, 3, 4, 7, 8], [2, 3, 5, 6]],
+            # [[], []],
             [[], [0]]]
 
 for csCnt, Lsts in enumerate(InputLst):
