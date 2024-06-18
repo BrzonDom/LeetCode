12b347ec9426ca96749a0dd74236e817a642f15c
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-06-18 15:09:39 +0200

    100. Liked 022 01Sub v01.01 Code selection

--- Top 100 Liked Questions/Submissions/022. Generate Parentheses_01Sub.py
+++ Top 100 Liked Questions/Submissions/022. Generate Parentheses_01Sub.py
@@ -67,56 +67,9 @@ def Sol01_BackTrackGlob_Prt(csNum):
     print("\n")
 
 
-def Sol01A_BackTrackGlob_Prt():
-
-    def backtrack(prthCs, lftPrth, rgtPrth, prthNum):
-
-        if len(prthCs) == 2 * prthNum:
-            prnthComb.append(prthCs)
-
-            return prnthComb
-
-        if lftPrth < prthNum:
-            backtrack(prthCs + '(', lftPrth + 1, rgtPrth, prthNum)
-
-        if rgtPrth < lftPrth:
-            backtrack(prthCs + ')', lftPrth, rgtPrth + 1, prthNum)
-
-    for csCnt in range(1, 9):
-
-        print(f"{csCnt}. Case\n")
-
-        print(f"\tNum. of parentheses: {csCnt}")
-
-        parentExamp = ""
-
-        for p in range(csCnt):
-            parentExamp += "()"
-
-        print(f"\t\tParentheses: {parentExamp}")
-        print()
-
-        prnthComb = []
-        backtrack("", 0, 0, csCnt)
-
-        print(f"\tNum. of valid combinations: {len(prnthComb)}")
-        print("\tValid combinations:")
-
-        print("\t\t", end="")
-        for c, comb in enumerate(prnthComb):
-            print(comb, end=", ")
-
-            if (c + 1) % 5 == 0:
-                print(f"\n\t\t", end="")
-
-        print("\n")
-
-
 if __name__ == '__main__':
 
     InputLst = [3, 1]
-
-    # Sol01A_BackTrackGlob_Prt()
 
     for csCnt in range(1, 9):
 
