9058e43a357f59f2ece8dad6a98925c91691d760
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-06-22 21:50:02 +0200

    100. Liked 024 01Sub v01.00

---
+++ Top 100 Liked Questions/Submissions/024. Swap Nodes in Pairs_01Sub.py
@@ -0,0 +1,245 @@
+"""
+24. Swap Nodes in Pairs
+
+https://leetcode.com/problems/swap-nodes-in-pairs/description/
+
+    Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without
+    modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+    Example 1:
+
+        Input: head = [1, 2, 3, 4]
+        Output: [2, 1, 4, 3]
+
+    Example 2:
+
+        Input: head = []
+        Output: []
+
+    Example 3:
+
+        Input: head = [1]
+        Output: [1]
+
+
+    Constraints:
+
+        The number of nodes in the list is in the range [0, 100].
+        0 <= Node.val <= 100
+
+"""
+
+
+class ListNode:
+    def __init__(self, val=0, nxt=None):
+        self.val = val
+        self.nxt = nxt
+
+    def valLst_Nd(self):
+        curNd = self
+        valLst = []
+
+        while curNd:
+            valLst.append(curNd.val)
+            curNd = curNd.nxt
+
+        return valLst
+
+
+class LinkList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, val):
+        newNd = ListNode(val)
+
+        if self.head is None:
+            self.head = newNd
+
+        else:
+            curNd = self.head
+
+            while curNd.nxt:
+                curNd = curNd.nxt
+            curNd.nxt = newNd
+
+    def valLst_Lnk(self, strNd=None):
+
+        if strNd is None:
+            strNd = self.head
+
+        curNd = strNd
+        valLst = []
+
+        while curNd:
+            valLst.append(curNd.val)
+            curNd = curNd.nxt
+
+        return valLst
+
+
+def Hlp01_Swp_PrvCur_Prt(swpVal, lnkLst):
+
+    HdNd = lnkLst.head
+    preHdNd = ListNode(0, HdNd)
+
+    valLft = swpVal
+    valRgt = swpVal + 1
+
+    idxLft = valLft - 1
+    idxRgt = valRgt - 1
+
+    cntLft = 0
+    cntRgt = 0
+
+    print(f"\t\tSwitch: {idxLft}.Node and {idxRgt}.Node")
+
+    prvLftNd = preHdNd
+    curLftNd = HdNd
+
+    while curLftNd is not None and cntLft != idxLft:
+        cntLft += 1
+
+        prvLftNd = curLftNd
+        curLftNd = curLftNd.nxt
+
+    prvRgtNd = preHdNd
+    curRgtNd = HdNd
+
+    while curRgtNd is not None and cntRgt != idxRgt:
+        cntRgt += 1
+
+        prvRgtNd = curRgtNd
+        curRgtNd = curRgtNd.nxt
+
+    if curLftNd:
+        print(f"\t\t\t{idxLft}.Node: {curLftNd.val}")
+    else:
+        print(f"\t\t\t{idxLft}.Node: None")
+
+    if curRgtNd:
+        print(f"\t\t\t{idxRgt}.Node: {curRgtNd.val}")
+    else:
+        print(f"\t\t\t{idxRgt}.Node: None")
+    print()
+
+    if valLft == valRgt:
+        return lnkLst
+
+    elif curLftNd is None or curRgtNd is None:
+        return lnkLst
+
+    elif curLftNd == HdNd:
+        curLftNd.nxt = curRgtNd.nxt
+        lnkLst.head = curRgtNd
+        curRgtNd.nxt = curLftNd
+
+    elif curRgtNd.nxt is None:
+        prvLftNd.nxt = curRgtNd
+        curRgtNd.nxt = curLftNd
+        curLftNd.nxt = None
+
+    else:
+        prvLftNd.nxt = curRgtNd
+        curLftNd.nxt = curRgtNd.nxt
+        curRgtNd.nxt = curLftNd
+
+    return lnkLst
+
+
+def Sol01A_SwpWhlLftRgt_Prt(InLst):
+
+    for csCnt, case in enumerate(InLst):
+
+        print(f"{csCnt+1}. Case\n")
+
+        lnkLst = LinkList()
+
+        for num in case:
+            lnkLst.append(num)
+
+        valLst = lnkLst.valLst_Lnk()
+
+        print(f"\tOrg Linked list: {valLst}")
+        print()
+
+        HdNd = lnkLst.head
+        prHdNd = ListNode(0, HdNd)
+
+        curNd = prHdNd
+
+        while curNd.nxt and curNd.nxt.nxt:
+
+            lftNd = curNd.nxt
+            rgtNd = curNd.nxt.nxt
+
+            lftNd.nxt = rgtNd.nxt
+
+            curNd.nxt = rgtNd
+            curNd.nxt.nxt = lftNd
+
+            curNd = curNd.nxt.nxt
+
+        lnkLst.head = prHdNd.nxt
+
+        valLst = lnkLst.valLst_Lnk()
+
+        print(f"\tOut Linked list: {valLst}")
+
+        print("\n")
+
+
+def Sol01_SwpWhlLftRgt_Prt(case):
+
+    lnkLst = LinkList()
+
+    for num in case:
+        lnkLst.append(num)
+
+    valLst = lnkLst.valLst_Lnk()
+
+    print(f"\tOrg Linked list: {valLst}")
+    print()
+
+    HdNd = lnkLst.head
+    prHdNd = ListNode(0, HdNd)
+
+    curNd = prHdNd
+
+    while curNd.nxt and curNd.nxt.nxt:
+
+        lftNd = curNd.nxt
+        rgtNd = curNd.nxt.nxt
+
+        lftNd.nxt = rgtNd.nxt
+
+        curNd.nxt = rgtNd
+        curNd.nxt.nxt = lftNd
+
+        curNd = curNd.nxt.nxt
+
+    lnkLst.head = prHdNd.nxt
+
+    valLst = lnkLst.valLst_Lnk()
+
+    print(f"\tOut Linked list: {valLst}")
+
+    print("\n")
+
+
+if __name__ == '__main__':
+
+    InputLst = [[1, 2, 3, 4],
+                [],
+                [1]]
+
+    # InputLst = [[1, 2, 3, 4, 5]]
+
+    # Sol01A_SwpWhlLftRgt_Prt(InputLst)
+
+    for csCnt, case in enumerate(InputLst):
+
+        print(f"{csCnt+1}. Case\n")
+
+        Sol01_SwpWhlLftRgt_Prt(case)
+
