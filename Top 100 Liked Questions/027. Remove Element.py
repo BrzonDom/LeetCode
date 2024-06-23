183064b1a3f0e0bc638e181c6c23f82508284860
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-06-23 16:48:07 +0200

    100. Liked 027 v01.01 Document formatting

--- Top 100 Liked Questions/027. Remove Element.py
+++ Top 100 Liked Questions/027. Remove Element.py
@@ -3,31 +3,34 @@
 
 https://leetcode.com/problems/remove-element/description/
 
-    Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+    Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the
+    elements may be changed. Then return the number of elements in nums which are not equal to val.
 
-    Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+    Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do
+    the following things:
 
         Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+
         Return k.
 
 
-    Custom Judge:
+        Custom Judge:
 
-        The judge will test your solution with the following code:
+            The judge will test your solution with the following code:
 
-        int[] nums = [...]; // Input array
-        int val = ...; // Value to remove
-        int[] expectedNums = [...]; // The expected answer with correct length.
-                                    // It is sorted with no values equaling val.
+            int[] nums = [...]; // Input array
+            int val = ...; // Value to remove
+            int[] expectedNums = [...]; // The expected answer with correct length.
+                                        // It is sorted with no values equaling val.
 
-        int k = removeElement(nums, val); // Calls your implementation
+            int k = removeElement(nums, val); // Calls your implementation
 
-        assert k == expectedNums.length;
-        sort(nums, 0, k); // Sort the first k elements of nums
-        for (int i = 0; i < actualLength; i++) {
-            assert nums[i] == expectedNums[i];
-        }
-        If all assertions pass, then your solution will be accepted.
+            assert k == expectedNums.length;
+            sort(nums, 0, k); // Sort the first k elements of nums
+            for (int i = 0; i < actualLength; i++) {
+                assert nums[i] == expectedNums[i];
+            }
+            If all assertions pass, then your solution will be accepted.
 
 
     Example 1:
@@ -36,7 +39,7 @@ https://leetcode.com/problems/remove-element/description/
         Output: 2, nums = [2,2,_,_]
 
         Explanation: Your function should return k = 2, with the first two elements of nums being 2.
-        It does not matter what you leave beyond the returned k (hence they are underscores).
+            It does not matter what you leave beyond the returned k (hence they are underscores).
 
     Example 2:
 
@@ -44,8 +47,8 @@ https://leetcode.com/problems/remove-element/description/
         Output: 5, nums = [0,1,4,0,3,_,_,_]
 
         Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
-        Note that the five elements can be returned in any order.
-        It does not matter what you leave beyond the returned k (hence they are underscores).
+            Note that the five elements can be returned in any order.
+            It does not matter what you leave beyond the returned k (hence they are underscores).
 
 
     Constraints:
