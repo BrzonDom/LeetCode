0a2c356833479b1eb29a2cbe6f29c0e62d899454
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-06-24 23:00:25 +0200

    100. Liked 027 01Sub v01.00

---
+++ Top 100 Liked Questions/Submissions/027. Remove Element_01Sub.py
@@ -0,0 +1,126 @@
+"""
+27. Remove Element
+
+https://leetcode.com/problems/remove-element/description/
+
+    Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the
+    elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+    Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do
+    the following things:
+
+        Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+
+        Return k.
+
+
+        Custom Judge:
+
+            The judge will test your solution with the following code:
+
+            int[] nums = [...]; // Input array
+            int val = ...; // Value to remove
+            int[] expectedNums = [...]; // The expected answer with correct length.
+                                        // It is sorted with no values equaling val.
+
+            int k = removeElement(nums, val); // Calls your implementation
+
+            assert k == expectedNums.length;
+            sort(nums, 0, k); // Sort the first k elements of nums
+            for (int i = 0; i < actualLength; i++) {
+                assert nums[i] == expectedNums[i];
+            }
+            If all assertions pass, then your solution will be accepted.
+
+
+    Example 1:
+
+        Input: nums = [3, 2, 2, 3], val = 3
+        Output: 2, nums = [2, 2, _, _]
+
+        Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+            It does not matter what you leave beyond the returned k (hence they are underscores).
+
+    Example 2:
+
+        Input: nums = [0, 1, 2, 2, 3, 0, 4, 2], val = 2
+        Output: 5, nums = [0, 1, 4, 0, 3, _, _, _]
+
+        Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+            Note that the five elements can be returned in any order.
+            It does not matter what you leave beyond the returned k (hence they are underscores).
+
+
+    Constraints:
+
+        0 <= nums.length <= 100
+        0 <= nums[i] <= 50
+        0 <= val <= 100
+
+"""
+
+
+def Sol01A_WhlPop_Prt(InLst):
+
+    for csCnt, case in enumerate(InLst):
+
+        print(f"{csCnt + 1}. Case\n")
+
+        inLst = case[0]
+        val = case[1]
+
+        print(f"\tInput List: {inLst}")
+        print(f"\t\tValue: {val}")
+        print()
+
+        nCnt = 0
+        while nCnt < len(inLst):
+
+            if inLst[nCnt] == val:
+                inLst.pop(nCnt)
+
+            else:
+                nCnt += 1
+
+        print(f"\tOutput List: {inLst}")
+
+        print("\n")
+
+
+def Sol01_WhlPop_Prt(case):
+
+    inLst = case[0]
+    val = case[1]
+
+    print(f"\tInput List: {inLst}")
+    print(f"\t\tValue: {val}")
+    print()
+
+    nCnt = 0
+    while nCnt < len(inLst):
+
+        if inLst[nCnt] == val:
+            inLst.pop(nCnt)
+
+        else:
+            nCnt += 1
+
+    print(f"\tOutput List: {inLst}")
+
+    print("\n")
+
+    return nCnt
+
+
+if __name__ == '__main__':
+
+    InputLst = [[[3, 2, 2, 3], 3],
+                [[0, 1, 2, 2, 3, 0, 4, 2], 2]]
+
+    # Sol01A_WhlPop_Prt(InputLst)
+
+    for csCnt, case in enumerate(InputLst):
+
+        print(f"{csCnt + 1}. Case\n")
+
+        Sol01_WhlPop_Prt(case)
