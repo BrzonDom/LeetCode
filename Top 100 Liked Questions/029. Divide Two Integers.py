d40a2131219c26ecf549b1834e6901eae060ff17
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-06-26 17:04:57 +0200

    100. Liked 029 v03.02 1.All function progress

--- Top 100 Liked Questions/029. Divide Two Integers.py
+++ Top 100 Liked Questions/029. Divide Two Integers.py
@@ -131,86 +131,88 @@ if __name__ == '__main__':
     InputLst = [[10, 3],
                 [7, -3]]
 
-    for csCnt, case in enumerate(InputLst):
+    Sol01A_WhlAdd_Prt(InputLst)
 
-        print(f"{csCnt + 1}. Case\n")
-
-        num = case[0]
-        div = case[1]
-
-        print(f"\tDividend: {num}")
-        print(f"\tDivisor:  {div}")
-        print()
-
-        if num:
-            if num < 0 and div < 0:
-                neg = False
-
-                num = abs(num)
-                div = abs(div)
-
-                print(f"\t\tPolarity:  1 ~ Positive")
-
-            elif num > 0 and div < 0:
-                neg = True
-
-                div = abs(div)
-
-                print(f"\t\tPolarity: -1 ~ Negative")
-
-            elif num < 0 and div > 0:
-                neg = True
-
-                num = abs(num)
-
-                print(f"\t\tPolarity: -1 ~ Negative")
-
-            else:
-                neg = False
-
-                print(f"\t\tPolarity:  1 ~ Positive")
-            print()
-
-            rem = num
-            quo = 0
-
-            while div <= rem:
-
-                rem -= div
-                quo += 1
-
-            print(f"\t\tQuotient:  {quo}")
-            print(f"\t\tRemainder: {rem}")
-            print()
-
-            if neg:
-                res = -quo
-
-            else:
-                res = quo
-
-            if 2 * rem >= div:
-
-                if neg:
-                    rnd = -(quo + 1)
-
-                else:
-                    rnd = quo + 1
-
-            else:
-                if neg:
-                    rnd = -quo
-
-                else:
-                    rnd = quo
-
-            print(f"\tResult: {res}")
-            print(f"\t\tRounded: {rnd}")
-
-        else:
-            print(f"\t\tDividend is 0")
-            print()
-
-            print(f"\tResult: 0")
-
-        print("\n")
+    # for csCnt, case in enumerate(InputLst):
+    #
+    #     print(f"{csCnt + 1}. Case\n")
+    #
+    #     num = case[0]
+    #     div = case[1]
+    #
+    #     print(f"\tDividend: {num}")
+    #     print(f"\tDivisor:  {div}")
+    #     print()
+    #
+    #     if num:
+    #         if num < 0 and div < 0:
+    #             neg = False
+    #
+    #             num = abs(num)
+    #             div = abs(div)
+    #
+    #             print(f"\t\tPolarity:  1 ~ Positive")
+    #
+    #         elif num > 0 and div < 0:
+    #             neg = True
+    #
+    #             div = abs(div)
+    #
+    #             print(f"\t\tPolarity: -1 ~ Negative")
+    #
+    #         elif num < 0 and div > 0:
+    #             neg = True
+    #
+    #             num = abs(num)
+    #
+    #             print(f"\t\tPolarity: -1 ~ Negative")
+    #
+    #         else:
+    #             neg = False
+    #
+    #             print(f"\t\tPolarity:  1 ~ Positive")
+    #         print()
+    #
+    #         rem = num
+    #         quo = 0
+    #
+    #         while div <= rem:
+    #
+    #             rem -= div
+    #             quo += 1
+    #
+    #         print(f"\t\tQuotient:  {quo}")
+    #         print(f"\t\tRemainder: {rem}")
+    #         print()
+    #
+    #         if neg:
+    #             res = -quo
+    #
+    #         else:
+    #             res = quo
+    #
+    #         if 2 * rem >= div:
+    #
+    #             if neg:
+    #                 rnd = -(quo + 1)
+    #
+    #             else:
+    #                 rnd = quo + 1
+    #
+    #         else:
+    #             if neg:
+    #                 rnd = -quo
+    #
+    #             else:
+    #                 rnd = quo
+    #
+    #         print(f"\tResult: {res}")
+    #         print(f"\t\tRounded: {rnd}")
+    #
+    #     else:
+    #         print(f"\t\tDividend is 0")
+    #         print()
+    #
+    #         print(f"\tResult: 0")
+    #
+    #     print("\n")
