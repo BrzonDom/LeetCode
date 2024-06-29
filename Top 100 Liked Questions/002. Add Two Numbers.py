d58260be3ea58f7972e905e7876dce853f6690e8
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-06-29 15:03:40 +0200

    100. Liked 002 v01.03 Document formatting

--- Top 100 Liked Questions/002. Add Two Numbers.py
+++ Top 100 Liked Questions/002. Add Two Numbers.py
@@ -3,7 +3,8 @@
 
 https://leetcode.com/problems/add-two-numbers/
 
-    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse
+    order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
     You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
@@ -40,6 +41,7 @@ InputLst = [[[2, 4, 3], [5, 6, 4]],
             [[9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9]]]
 
 for csCnt, case in enumerate(InputLst):
+
     print(f"{csCnt + 1}. Case\n")
 
     print(f"\t1.List: {case[0]}")
