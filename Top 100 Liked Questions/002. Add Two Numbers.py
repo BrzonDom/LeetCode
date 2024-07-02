07b59f3cf6ed73edf86c358f5f5ade625f022a41
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-07-02 17:38:12 +0200

    100. Liked 002 v04.10 1.Function progress

--- Top 100 Liked Questions/002. Add Two Numbers.py
+++ Top 100 Liked Questions/002. Add Two Numbers.py
@@ -303,4 +303,10 @@ if __name__ == '__main__':
                 [[0], [0]],
                 [[9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9]]]
 
-    Sol01A_WhlIndv_Prt(InputLst)
+    # Sol01A_WhlIndv_Prt(InputLst)
+
+    for csCnt, case in enumerate(InputLst):
+
+        print(f"{csCnt + 1}. Case\n")
+
+        Sol01_WhlIndv_Prt(case)
