a0ce5e68f1bd241252b78179e7fe543bb046b743
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-07-02 17:39:18 +0200

    100. Liked 002 v04.11 1.Function done

--- Top 100 Liked Questions/002. Add Two Numbers.py
+++ Top 100 Liked Questions/002. Add Two Numbers.py
@@ -193,10 +193,10 @@ def Sol01A_WhlIndv_Prt(InLst):
         print("\n")
 
 
-def Sol01_WhlIndv_Prt(case):
+def Sol01_WhlIndv_Prt(lst1, lst2):
 
-    lst1 = case[0]
-    lst2 = case[1]
+    # lst1 = case[0]
+    # lst2 = case[1]
 
     print(f"\t1. List: {lst1}")
     print(f"\t2. List: {lst2}")
@@ -309,4 +309,4 @@ if __name__ == '__main__':
 
         print(f"{csCnt + 1}. Case\n")
 
-        Sol01_WhlIndv_Prt(case)
+        Sol01_WhlIndv_Prt(case[0], case[1])
