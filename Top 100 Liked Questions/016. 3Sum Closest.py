ddfad657dc3751acc7a05504eb7c367ecbdaa1f7
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-07-03 11:40:06 +0200

    100. Liked 016 v01.03 Document formatting

--- Top 100 Liked Questions/016. 3Sum Closest.py
+++ Top 100 Liked Questions/016. 3Sum Closest.py
@@ -12,14 +12,14 @@ https://leetcode.com/problems/3sum-closest/description/
 
     Example 1:
 
-        Input: nums = [-1,2,1,-4], target = 1
+        Input: nums = [-1, 2, 1, -4], target = 1
         Output: 2
 
         Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 
     Example 2:
 
-        Input: nums = [0,0,0], target = 1
+        Input: nums = [0, 0, 0], target = 1
         Output: 0
 
         Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
@@ -33,8 +33,8 @@ https://leetcode.com/problems/3sum-closest/description/
 
 """
 
-InputLst = [[[-1,2,1,-4], 1],
-            [[0,0,0], 1]]
+InputLst = [[[-1, 2, 1, -4], 1],
+            [[0, 0, 0], 1]]
 
 for csCnt, case in enumerate(InputLst):
 
