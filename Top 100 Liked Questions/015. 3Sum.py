52b1710ad4d1b456f9ed5d31bb49062a44cbeba1
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-07-08 22:49:10 +0200

    100. Liked 015 v05.01 2.Function progress

--- Top 100 Liked Questions/015. 3Sum.py
+++ Top 100 Liked Questions/015. 3Sum.py
@@ -141,37 +141,39 @@ if __name__ == "__main__":
 
         print(f"{csCnt + 1}. Case:\n")
 
-        Nums = case
+        Sol02_TlCmb_Prt(case)
 
-        print(f"\tNums: {Nums}")
-        print()
-
-        trpCmb = combinations(Nums, 3)
-        trpCmb = map(list, trpCmb)
-
-        outCmb = []
-
-        print(f"\t\tAll combinations:")
-        for cmbCnt, cmb in enumerate(trpCmb):
-            ttl = sum(cmb)
-
-            if ttl == 0:
-                print()
-                print(f"\t\t\t{cmbCnt + 1}.Cmb.: \t{cmb} = {ttl}")
-                print()
-
-                outCmb.append(cmb)
-
-            else:
-                print(f"\t\t\t\t{cmbCnt + 1}.Cmb.: \t{cmb} = {ttl}")
-
-        print()
-
-        print("\tOut combinations:")
-        for cmb in outCmb:
-            print(f"\t\t{cmb}")
-
-        print("\n")
+        # Nums = case
+        #
+        # print(f"\tNums: {Nums}")
+        # print()
+        #
+        # trpCmb = combinations(Nums, 3)
+        # trpCmb = map(list, trpCmb)
+        #
+        # outCmb = []
+        #
+        # print(f"\t\tAll combinations:")
+        # for cmbCnt, cmb in enumerate(trpCmb):
+        #     ttl = sum(cmb)
+        #
+        #     if ttl == 0:
+        #         print()
+        #         print(f"\t\t\t{cmbCnt + 1}.Cmb.: \t{cmb} = {ttl}")
+        #         print()
+        #
+        #         outCmb.append(cmb)
+        #
+        #     else:
+        #         print(f"\t\t\t\t{cmbCnt + 1}.Cmb.: \t{cmb} = {ttl}")
+        #
+        # print()
+        #
+        # print("\tOut combinations:")
+        # for cmb in outCmb:
+        #     print(f"\t\t{cmb}")
+        #
+        # print("\n")
 
         # outRtrn = Sol01_TrpFrLp_Prt(case)
         #
