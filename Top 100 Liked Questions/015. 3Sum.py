ebdb7cb64c871b00ac7246854e343a9bc93fe12e
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-07-10 16:58:40 +0200

    100. Liked 015 v07.02 Code selection

--- Top 100 Liked Questions/015. 3Sum.py
+++ Top 100 Liked Questions/015. 3Sum.py
@@ -194,58 +194,6 @@ if __name__ == "__main__":
 
         Sol03_StrPntr_Prt(case)
 
-        # Nums = case
-        # print(f"\tNums unsorted: {Nums}")
-        #
-        # Nums.sort()
-        # print(f"\tNums sorted:   {Nums}")
-        #
-        # lenNum = len(Nums)
-        # print(f"\t\tNums size: {lenNum}")
-        # print()
-        #
-        # outCmb = []
-        #
-        # print(f"\t\tAll combinations:")
-        # for st in range(lenNum):
-        #     nSt = Nums[st]
-        #
-        #     lf = st + 1
-        #     rg = lenNum - 1
-        #
-        #     while lf < rg:
-        #         nLf = Nums[lf]
-        #         nRg = Nums[rg]
-        #
-        #         cmb = [nSt, nLf, nRg]
-        #         ttl = sum(cmb)
-        #
-        #         if ttl < 0:
-        #             lf += 1
-        #
-        #             print(f"\t\t\t\t{cmb} = {ttl}")
-        #
-        #         elif ttl > 0:
-        #             rg -= 1
-        #
-        #             print(f"\t\t\t\t{cmb} = {ttl}")
-        #
-        #         else:
-        #             if cmb not in outCmb:
-        #                 outCmb.append(cmb)
-        #
-        #             lf += 1
-        #             rg -= 1
-        #
-        #             print(f"\t\t\t{cmb}")
-        # print()
-        #
-        # print(f"\tOut combinations:")
-        # for cmb in outCmb:
-        #     print(f"\t\t{cmb}")
-        #
-        # print("\n")
-
         # outRtrn = Sol01_TrpFrLp_Prt(case)
 
         # outRtrn = Sol02_TlCmb_Prt(case)
