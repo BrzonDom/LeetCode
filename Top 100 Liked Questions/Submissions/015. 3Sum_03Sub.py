c0b7f2555b997e6d0e16f21c0912885a24658b96
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-07-15 14:45:18 +0200

    100. Liked 015 03Sub v03.01 Problem input

--- Top 100 Liked Questions/Submissions/015. 3Sum_03Sub.py
+++ Top 100 Liked Questions/Submissions/015. 3Sum_03Sub.py
@@ -43,3 +43,14 @@ https://leetcode.com/problems/3sum/description/
         -10^5 <= nums[i] <= 10^5
 
 """
+
+
+InputLst = [[-1, 0, 1, 2, -1, -4],
+            [0, 1, 1],
+            [0, 0, 0],
+            [-1, -2, -3, -4, -5, 0, 1, 4, 5, 2]]
+
+for case in InputLst:
+
+    print(f"Input: {case}")
+    print()
