6bd07e0646ba29790eb472806d67853f936a0f44
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-07-15 14:50:10 +0200

    100. Liked 015 03Sub v03.02 Input print progress

--- Top 100 Liked Questions/Submissions/015. 3Sum_03Sub.py
+++ Top 100 Liked Questions/Submissions/015. 3Sum_03Sub.py
@@ -50,7 +50,10 @@ InputLst = [[-1, 0, 1, 2, -1, -4],
             [0, 0, 0],
             [-1, -2, -3, -4, -5, 0, 1, 4, 5, 2]]
 
-for case in InputLst:
+for csCnt, case in enumerate(InputLst):
 
-    print(f"Input: {case}")
-    print()
+    print(f"{csCnt + 1}. Case:\n")
+
+    print(f"\tNums: {case}")
+
+    print("\n")
