30247cf5c9ecf53a2fccd56836530ad87d87e708
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-07-15 14:56:41 +0200

    100. Liked 015 03Sub v03.04 Case assignment

--- Top 100 Liked Questions/Submissions/015. 3Sum_03Sub.py
+++ Top 100 Liked Questions/Submissions/015. 3Sum_03Sub.py
@@ -56,6 +56,7 @@ if __name__ == "__main__":
 
         print(f"{csCnt + 1}. Case:\n")
 
-        print(f"\tNums: {case}")
+        Nums = case
+        print(f"\tNums: {Nums}")
 
         print("\n")
