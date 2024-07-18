28fb7b2f5e4972fa98f94b0b1dbe9c52d89fc844
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-07-18 21:53:18 +0200

    100. Liked 018 v05.28 Document formatting

--- Top 100 Liked Questions/018. 4Sum.py
+++ Top 100 Liked Questions/018. 4Sum.py
@@ -232,12 +232,8 @@ if __name__ == "__main__":
 
         outRtrn = Sol01_DblFrPntMtd_Ptr(case[0], case[1])
 
-        Out_Prt(outRtrn)
-
-        print("\n")
-
         # outRtrn = Brt01_QdrFrLp_Prt(case[0], case[1])
 
         # outRtrn = Brt02_TlCmb_Prt(case[0], case[1])
 
-        # Out_Prt(outRtrn)
+        Out_Prt(outRtrn)
