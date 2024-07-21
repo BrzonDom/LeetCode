db744195f1c9c5ce14893be34e266e936bc43851
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-07-21 23:30:49 +0200

    100. Liked 018 v07.25 Helper progress

--- Top 100 Liked Questions/018. 4Sum.py
+++ Top 100 Liked Questions/018. 4Sum.py
@@ -216,7 +216,9 @@ def Hlp02_nSumPntMtd_Prt(shftCr, rmNm, cmbCr, trgtCr):
                     outCmb.append(cmbNx)
 
                 lf += 1
-                rg -= 1
+
+                while nLf == Nums[lf] and lf < rg:
+                    lf += 1
 
                 print()
                 print(f"\t\t\t{cmbNx}")
