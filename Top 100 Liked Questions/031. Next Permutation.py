41497d6e4ef89cd5ac831bca4b2e9392e48af365
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-07-27 22:33:28 +0200

    100. Liked 031 v03.27 Print progress

--- Top 100 Liked Questions/031. Next Permutation.py
+++ Top 100 Liked Questions/031. Next Permutation.py
@@ -111,14 +111,19 @@ def Sol01_RvsSwtch_Prt(Prm):
         if pNm > nm:
 
             if pNm > ppNm:
+                print(f"\t\tPrPr. Num.: {ppNm} = {Prm[-i]}")
+                print(f"\t\tPr. Num.:   {pNm} = {Prm[-i-1]}")
+                print(f"\t\tCr. Num.:   {nm} = {Prm[-i-2]}")
+                print()
+
                 Prm[-i], Prm[-i - 2] = Prm[-i - 2], Prm[-i]
 
             else:
-                Prm[-i - 1], Prm[-i - 2] = Prm[-i - 2], Prm[-i - 1]
+                print(f"\t\tPr. Num.: {pNm}")
+                print(f"\t\tCr. Num.: {nm}")
+                print()
 
-            print(f"\t\tPr. Num.: {pNm}")
-            print(f"\t\tCr. Num.: {nm}")
-            print()
+                Prm[-i - 1], Prm[-i - 2] = Prm[-i - 2], Prm[-i - 1]
 
             mxPrm = False
             break
