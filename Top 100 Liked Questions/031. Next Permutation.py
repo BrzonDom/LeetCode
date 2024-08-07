3e3c0a71980249a35541d3ea37cafc583b2aaf41
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-08-07 23:19:12 +0200

    100. Liked 031 v03.45 1.Solution function progress

--- Top 100 Liked Questions/031. Next Permutation.py
+++ Top 100 Liked Questions/031. Next Permutation.py
@@ -119,13 +119,24 @@ def Sol01_RvsSwtch_Prt(Prm):
 
         if pNm > nm:
 
-            print()
-            print(f"\t\tPrv. Num.: {Prm[-(i+1)]}")
-            print(f"\t\tCrn. Num.: {Prm[-(i+2)]}")
-            print()
+            if pNm > ppNm > nm:
+                print()
+                print(f"\t\tPrPrv. Num.: {Prm[-i]}")
+                print(f"\t\tPrv. Num.:   {Prm[-(i+1)]}")
+                print(f"\t\tCrn. Num.:   {Prm[-(i+2)]}")
+                print()
 
-            Prm[-(i+1)], Prm[-(i+2)] = Prm[-(i+2)], Prm[-(i+1)]
-            Prm[-(i+1):] = sorted(Prm[-(i+1):])
+                Prm[-i], Prm[-(i+2)] = Prm[-(i+2)], Prm[-i]
+                Prm[-(i+1):] = sorted(Prm[-(i+1):])
+
+            else:
+                print()
+                print(f"\t\tPrv. Num.: {Prm[-(i+1)]}")
+                print(f"\t\tCrn. Num.: {Prm[-(i+2)]}")
+                print()
+
+                Prm[-(i+1)], Prm[-(i+2)] = Prm[-(i+2)], Prm[-(i+1)]
+                Prm[-(i+1):] = sorted(Prm[-(i+1):])
 
             mxPrm = False
             break
