b04e74a5d5078808d3af53622d29739037e01823
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-08-17 16:50:39 +0200

    100. Liked 034 v02.21 Code selection

--- Top 100 Liked Questions/034. Find First and Last Position of Element in Sorted Array.py
+++ Top 100 Liked Questions/034. Find First and Last Position of Element in Sorted Array.py
@@ -97,18 +97,4 @@ for csCnt, case in enumerate(InputLst):
 
         En = Md - 1
 
-    if St <= En:
-
-        Md = (St + En) // 2
-
-        nSt = Arr[St]
-        nEn = Arr[En]
-        nMd = Arr[Md]
-
-        stpCnt += 1
-
-        print(f"\t\t{stpCnt}. Start: {nSt} = Arr[{St}]")
-        print(f"\t\t{stpCnt}. End:   {nEn} = Arr[{En}]")
-        print(f"\t\t{stpCnt}. Mid:   {nMd} = Arr[{Md}]")
-
     print("\n")
