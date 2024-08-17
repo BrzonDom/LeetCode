586b3500c14ef605484b06e871d4c6a5b029cab5
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-08-17 14:36:42 +0200

    100. Liked 034 v01.01 Document formatting

--- Top 100 Liked Questions/034. Find First and Last Position of Element in Sorted Array.py
+++ Top 100 Liked Questions/034. Find First and Last Position of Element in Sorted Array.py
@@ -4,7 +4,8 @@
 https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
 
 
-    Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+    Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of
+    a given target value.
 
     If target is not found in the array, return [-1, -1].
 
@@ -13,25 +14,25 @@ https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-
 
     Example 1:
 
-        Input: nums = [5,7,7,8,8,10], target = 8
-        Output: [3,4]
+        Input: nums = [5, 7, 7, 8, 8, 10], target = 8
+        Output: [3, 4]
 
     Example 2:
 
-        Input: nums = [5,7,7,8,8,10], target = 6
-        Output: [-1,-1]
+        Input: nums = [5, 7, 7, 8, 8, 10], target = 6
+        Output: [-1, -1]
 
     Example 3:
 
         Input: nums = [], target = 0
-        Output: [-1,-1]
+        Output: [-1, -1]
 
 
     Constraints:
 
-        0 <= nums.length <= 105
-        -109 <= nums[i] <= 109
+        0 <= nums.length <= 10^5
+        -10^9 <= nums[i] <= 10^9
         nums is a non-decreasing array.
-        -109 <= target <= 109
+        -10^9 <= target <= 10^9
 
 """
