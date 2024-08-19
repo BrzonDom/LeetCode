8dc6ba6526ad6e3e41dc96c1411977e69f7a9261
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-08-19 20:54:35 +0200

    100. Liked 036 v01.08 Print progress

--- Top 100 Liked Questions/036. Valid Sudoku.py
+++ Top 100 Liked Questions/036. Valid Sudoku.py
@@ -80,6 +80,8 @@ for csCnt, case in enumerate(InputLst):
 
     print(f"{csCnt + 1}.Case\n")
 
+
+    print("\tSudoku:")
     for row in case:
         print("\n\t\t", end="")
         for col in row:
