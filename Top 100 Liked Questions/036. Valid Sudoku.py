ca67b7547715099788767db6f09f287caf672cc4
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-08-19 21:58:11 +0200

    100. Liked 036 v02.29 Driver update

--- Top 100 Liked Questions/036. Valid Sudoku.py
+++ Top 100 Liked Questions/036. Valid Sudoku.py
@@ -167,13 +167,15 @@ for csCnt, case in enumerate(InputLst):
                             boxs[b] += col
 
                     elif c <= 5:
-
                         b = 5
 
                         if b not in boxs:
                             boxs[b] = col
 
                         else:
+                            if col in boxs[b]:
+                                Valid = False
+
                             boxs[b] += col
 
                     elif c <= 8:
