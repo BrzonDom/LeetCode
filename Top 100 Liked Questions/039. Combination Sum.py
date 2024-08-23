31fd77e858cfc13966c98cbddb48b11263273b85
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-08-23 22:33:12 +0200

    100. Liked 039 v02.01 Print progress

--- Top 100 Liked Questions/039. Combination Sum.py
+++ Top 100 Liked Questions/039. Combination Sum.py
@@ -52,7 +52,7 @@ for csCnt, case in enumerate(InputLst):
 
     print(f"{csCnt + 1}.Case\n")
 
-    print(f"\t{case[0]}")
-    print(f"\t{case[1]}")
+    print(f"\tCandidates: {case[0]}")
+    print(f"\tTarget:      {case[1]}")
 
     print("\n")
