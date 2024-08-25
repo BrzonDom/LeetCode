ca3413ff615be40fc0ddff163ad6cda0d9f523ba
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-08-25 20:36:39 +0200

    100. Liked 037 v02.04 Driver progress

--- Top 100 Liked Questions/037. Sudoku Solver.py
+++ Top 100 Liked Questions/037. Sudoku Solver.py
@@ -64,8 +64,10 @@ for csCnt, case in enumerate(InputLst):
 
     print(f"{csCnt+1}.Case\n")
 
+    Brd = case
+
     print(f"\tSet Board:")
-    for line in case:
+    for line in Brd:
         print(f"\t\t", end="")
 
         for num in line:
