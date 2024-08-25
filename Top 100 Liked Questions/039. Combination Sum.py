d8ec1660bf479e1c86b6388c0c0b0d6b5fdacea9
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-08-25 15:06:08 +0200

    100. Liked 039 v05.19 2.Solution function update

--- Top 100 Liked Questions/039. Combination Sum.py
+++ Top 100 Liked Questions/039. Combination Sum.py
@@ -163,17 +163,7 @@ def Slt02_VstdCmbFnc_Prt(Cnds, Trg):
         if not fndCmb:
             print()
 
-        if Cmbs:
-            print("\tCombinations:")
-
-            for c, cmb in enumerate(Cmbs):
-                print(f"\t\t{c+1}. {cmb}")
-
-            return Cmbs
-
-    print("\tNo Combinations Found")
-
-    return []
+    return Cmbs
 
 
 def Out_Prt(Cmbs):
