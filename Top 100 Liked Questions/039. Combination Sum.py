32bee5d996155f64beb4a4fa1af84dfb9b1987c0
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-08-26 19:23:16 +0200

    100. Liked 039 v06.14 Print update

--- Top 100 Liked Questions/039. Combination Sum.py
+++ Top 100 Liked Questions/039. Combination Sum.py
@@ -219,10 +219,11 @@ if __name__ == "__main__":
             Hlp03_BckTrckCndRcrs_Prt([nm], nm)
         print()
 
-        print("\tCombinations:")
+        if Cmbs:
+            print("\tCombinations:")
 
-        for cmb in Cmbs:
-            print(f"\t\t{cmb}")
+            for cmb in Cmbs:
+                print(f"\t\t{cmb}")
 
         # Out_Prt(outRtrn)
 
