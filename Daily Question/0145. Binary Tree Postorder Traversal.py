29c1ce2172c1b9db62fceea3972da0b4c5614548
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-08-27 23:31:51 +0200

    Daily 0145 v02.01 Setup progress

--- Daily Question/0145. Binary Tree Postorder Traversal.py
+++ Daily Question/0145. Binary Tree Postorder Traversal.py
@@ -42,6 +42,13 @@ https://leetcode.com/problems/binary-tree-postorder-traversal/description/?envTy
 """
 
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 if __name__ == "__main__":
 
     InputLst = [[1, 0, 2, 3],
