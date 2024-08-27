2465f03c1912ab81004c38ed78bd26a23071fcc9
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-08-27 23:29:18 +0200

    Daily 0145 v01.06 Comment

--- Daily Question/0145. Binary Tree Postorder Traversal.py
+++ Daily Question/0145. Binary Tree Postorder Traversal.py
@@ -31,6 +31,16 @@ https://leetcode.com/problems/binary-tree-postorder-traversal/description/?envTy
 
 """
 
+"""
+    Definition for a binary tree node:
+    
+    class TreeNode:
+        def __init__(self, val=0, left=None, right=None):
+            self.val = val
+            self.left = left
+            self.right = right
+"""
+
 
 InputLst = [[1, 0, 2, 3],
             [],
