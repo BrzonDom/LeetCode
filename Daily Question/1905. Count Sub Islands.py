0742374c5f415063ee3e1af62225572d887323a5
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-08-28 14:00:40 +0200

    Daily 1905 v01.05 Driver print progress

--- Daily Question/1905. Count Sub Islands.py
+++ Daily Question/1905. Count Sub Islands.py
@@ -86,9 +86,11 @@ InputLst = [[
          [1, 0, 0, 0, 1]]
             ]]
 
-for case in InputLst:
+for csCnt, case in enumerate(InputLst):
+
+    print(f"{csCnt + 1}.Case\n")
 
     print(f"\t{case[0]}")
     print(f"\t{case[1]}")
 
-    print()
+    print("\n")
