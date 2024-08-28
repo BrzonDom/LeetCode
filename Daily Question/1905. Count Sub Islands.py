a7725e17a59030b1cbd07cea8c4d5892b87990f5
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-08-28 18:33:29 +0200

    Daily 1905 v03.14 Driver progress

--- Daily Question/1905. Count Sub Islands.py
+++ Daily Question/1905. Count Sub Islands.py
@@ -171,24 +171,26 @@ if __name__ == "__main__":
         RwDm = len(Lnd)
         ClDm = len(Lnd[0])
 
-        print("\tLand grid:")
+        Prt02_Grd_DspIn(Lnd, 0)
 
-        print("\t\t\t\t", end="   ")
-        for c in range(1, ClDm+1):
-            print(f"{c:2}", end=" ")
-        print()
-
-        for r, ln in enumerate(Lnd):
-            print(f"\t\t\t\t{r+1:2}", end=" ")
-
-            for sq in ln:
-
-                if sq:
-                    print("[#]", end="")
-                else:
-                    print("[ ]", end="")
-            print()
-        print()
+        # print("\tLand grid:")
+        #
+        # print("\t\t\t\t", end="   ")
+        # for c in range(1, ClDm+1):
+        #     print(f"{c:2}", end=" ")
+        # print()
+        #
+        # for r, ln in enumerate(Lnd):
+        #     print(f"\t\t\t\t{r+1:2}", end=" ")
+        #
+        #     for sq in ln:
+        #
+        #         if sq:
+        #             print("[#]", end="")
+        #         else:
+        #             print("[ ]", end="")
+        #     print()
+        # print()
 
         print("\tIsland grid:")
 
