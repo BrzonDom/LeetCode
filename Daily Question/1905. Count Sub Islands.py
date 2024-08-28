84e46cc685862e9ace151e5c554cd3d65f27832a
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-08-28 18:33:47 +0200

    Daily 1905 v03.15 Code selection

--- Daily Question/1905. Count Sub Islands.py
+++ Daily Question/1905. Count Sub Islands.py
@@ -173,25 +173,6 @@ if __name__ == "__main__":
 
         Prt02_Grd_DspIn(Lnd, 0)
 
-        # print("\tLand grid:")
-        #
-        # print("\t\t\t\t", end="   ")
-        # for c in range(1, ClDm+1):
-        #     print(f"{c:2}", end=" ")
-        # print()
-        #
-        # for r, ln in enumerate(Lnd):
-        #     print(f"\t\t\t\t{r+1:2}", end=" ")
-        #
-        #     for sq in ln:
-        #
-        #         if sq:
-        #             print("[#]", end="")
-        #         else:
-        #             print("[ ]", end="")
-        #     print()
-        # print()
-
         print("\tIsland grid:")
 
         print("\t\t\t\t", end="   ")
