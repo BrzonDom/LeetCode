876957a2a60eaa4524f6d64e65b5ad0fa82b9275
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-08-30 17:47:04 +0200

    Daily 1905 v06.27 Comment

--- Daily Question/1905. Count Sub Islands.py
+++ Daily Question/1905. Count Sub Islands.py
@@ -3,6 +3,8 @@
 
 https://leetcode.com/problems/count-sub-islands/description/?envType=daily-question&envId=2024-08-28
 
+        Day: 2024/08/28
+
     You are given two m x n binary matrices grid1 and grid2 containing only 0's (representing water) and 1's
     (representing land). An island is a group of 1's connected 4-directionally (horizontal or vertical). Any cells
     outside of the grid are considered water cells.
