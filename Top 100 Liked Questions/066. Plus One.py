ce51a4726f84d847767e3205497e93c7956335a9
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-08-31 23:13:00 +0200

    100. Liked 066 v01.03 Document format

--- Top 100 Liked Questions/066. Plus One.py
+++ Top 100 Liked Questions/066. Plus One.py
@@ -12,33 +12,33 @@ https://leetcode.com/problems/plus-one/description/
 
     Example 1:
 
-        Input: digits = [1,2,3]
+        Input: digits = [1, 2, 3]
 
-        Output: [1,2,4]
+        Output: [1, 2, 4]
 
         Explanation: The array represents the integer 123.
             Incrementing by one gives 123 + 1 = 124.
-            Thus, the result should be [1,2,4].
+            Thus, the result should be [1, 2, 4].
 
     Example 2:
 
-        Input: digits = [4,3,2,1]
+        Input: digits = [4, 3, 2, 1]
 
-        Output: [4,3,2,2]
+        Output: [4, 3, 2, 2]
 
         Explanation: The array represents the integer 4321.
             Incrementing by one gives 4321 + 1 = 4322.
-            Thus, the result should be [4,3,2,2].
+            Thus, the result should be [4, 3, 2, 2].
 
     Example 3:
 
         Input: digits = [9]
 
-        Output: [1,0]
+        Output: [1, 0]
 
         Explanation: The array represents the integer 9.
             Incrementing by one gives 9 + 1 = 10.
-            Thus, the result should be [1,0].
+            Thus, the result should be [1, 0].
 
 
     Constraints:
@@ -50,8 +50,8 @@ https://leetcode.com/problems/plus-one/description/
 """
 
 
-InputLst = [[1,2,3],
-            [4,3,2,1],
+InputLst = [[1, 2, 3],
+            [4, 3, 2, 1],
             [9]]
 
 for case in InputLst:
