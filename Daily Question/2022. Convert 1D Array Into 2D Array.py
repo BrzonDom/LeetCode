a2a680e0cd36894c8e2212a5cb5aaf77db582b4d
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-01 21:37:22 +0200

    Daily 2022 v01.03 Document format

--- Daily Question/2022. Convert 1D Array Into 2D Array.py
+++ Daily Question/2022. Convert 1D Array Into 2D Array.py
@@ -15,26 +15,26 @@ https://leetcode.com/problems/convert-1d-array-into-2d-array/description/?envTyp
 
     Example 1:
 
-        Input: original = [1,2,3,4], m = 2, n = 2
+        Input: original = [1, 2, 3, 4], m = 2, n = 2
 
-        Output: [[1,2],[3,4]]
+        Output: [[1, 2], [3, 4]]
 
         Explanation: The constructed 2D array should contain 2 rows and 2 columns.
-            The first group of n=2 elements in original, [1,2], becomes the first row in the constructed 2D array.
-            The second group of n=2 elements in original, [3,4], becomes the second row in the constructed 2D array.
+            The first group of n=2 elements in original, [1, 2], becomes the first row in the constructed 2D array.
+            The second group of n=2 elements in original, [3, 4], becomes the second row in the constructed 2D array.
 
     Example 2:
 
-        Input: original = [1,2,3], m = 1, n = 3
+        Input: original = [1, 2, 3], m = 1, n = 3
 
-        Output: [[1,2,3]]
+        Output: [[1, 2, 3]]
 
         Explanation: The constructed 2D array should contain 1 row and 3 columns.
             Put all three elements in original into the first row of the constructed 2D array.
 
     Example 3:
 
-        Input: original = [1,2], m = 1, n = 1
+        Input: original = [1, 2], m = 1, n = 1
 
         Output: []
 
@@ -51,9 +51,9 @@ https://leetcode.com/problems/convert-1d-array-into-2d-array/description/?envTyp
 """
 
 
-InputLst = [[[1,2,3,4], 2, 2],
-            [[1,2,3], 1, 3],
-            [[1,2], 1, 1]]
+InputLst = [[[1, 2, 3, 4], 2, 2],
+            [[1, 2, 3], 1, 3],
+            [[1, 2], 1, 1]]
 
 for case in InputLst:
 
