5ca68c66e176ca270898bce96102cd5f72884e63
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-02 23:08:40 +0200

    100. Liked 067 v02.02 Driver progress

--- Top 100 Liked Questions/067. Add Binary.py
+++ Top 100 Liked Questions/067. Add Binary.py
@@ -44,4 +44,7 @@ if __name__ == "__main__":
         print(f"\t1. Binary: {Bn1}")
         print(f"\t2. Binary: {Bn2}")
 
+        bn1 = int(Bn1, 2)
+        bn2 = int(Bn2, 2)
+
         print("\n")
