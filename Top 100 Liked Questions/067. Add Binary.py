bb042f518b3bb8b7902a615af281cf68c457bcfc
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-02 23:30:23 +0200

    100. Liked 067 v02.18 1.Solution function print progress

--- Top 100 Liked Questions/067. Add Binary.py
+++ Top 100 Liked Questions/067. Add Binary.py
@@ -40,6 +40,9 @@ def Slt01_BldBnTls_Prt(Bn1, Bn2):
     bn1 = bin(nm1)
     bn2 = bin(nm2)
 
+    print(f"\t\t1. Int Binary: {bn1[2:]}")
+    print(f"\t\t2. Int Binary: {bn2[2:]}")
+
     return
 
 
