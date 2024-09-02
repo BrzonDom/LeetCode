1f5bbffd44abb7e12133697ba9607e45d5911ae7
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-02 23:18:44 +0200

    100. Liked 067 v02.09 Driver print progress

--- Top 100 Liked Questions/067. Add Binary.py
+++ Top 100 Liked Questions/067. Add Binary.py
@@ -53,7 +53,10 @@ if __name__ == "__main__":
 
         print(f"\t\t1. Int Binary: {bn1[2:]} = {nm1}")
         print(f"\t\t2. Int Binary: {bn2[2:]} = {nm2}")
+        print()
 
         bnRs = bin(nm1 + nm2)
+
+        print(f"\tBinary result: {bnRs}")
 
         print("\n")
