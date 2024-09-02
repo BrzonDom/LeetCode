52923fc34a3fe09f10083f7b586fa7fcb47cd0cc
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-02 17:56:56 +0200

    Daily 1894 v02.21 1.Brute function progress

--- Daily Question/1894. Find the Student that Will Replace the Chalk.py
+++ Daily Question/1894. Find the Student that Will Replace the Chalk.py
@@ -72,7 +72,9 @@ def Brt01_WhlFrChkSbt_Prt(Stds, Chk):
 
     print("\tStudents chalk usage:")
     for idx, std in enumerate(Stds):
-        print(f"\t\t{idx}. Student: {std} chalk")
+        Chk -= std
+
+        print(f"\t\t{idx}. Student: Chalk Used {std} / Remaining {Chk}")
 
     return
 
