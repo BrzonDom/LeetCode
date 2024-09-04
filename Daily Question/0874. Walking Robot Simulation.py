df8a2a8e0c5a2e480dfe52e2917cf1841e9c5219
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-04 20:39:18 +0200

    Daily 0874 v02.35 Driver upgrade

--- Daily Question/0874. Walking Robot Simulation.py
+++ Daily Question/0874. Walking Robot Simulation.py
@@ -102,6 +102,12 @@ if __name__ == "__main__":
 
         print(f"{csCnt + 1}.Case\n")
 
+        Slt01_DctItrMv_Prt(case[0], case[1])
+
+        print("\n")
+
+        continue
+
         Movs = case[0]
         Obts = case[1]
 
