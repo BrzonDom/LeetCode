04f6821a83bd159da574f421ddfe207dae4972e2
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-04 18:39:56 +0200

    Daily 0874 v02.23 Driver update

--- Daily Question/0874. Walking Robot Simulation.py
+++ Daily Question/0874. Walking Robot Simulation.py
@@ -115,18 +115,26 @@ if __name__ == "__main__":
                             break
 
                         rbt[0][1] += 1
-                        dst += 1
 
                 elif rbt[1] == 'E':
                     for _ in range(mv):
+                        if [rbt[0][0] + 1, rbt[0][1]] in Obts:
+                            break
+
                         rbt[0][0] += 1
 
                 elif rbt[1] == 'S':
                     for _ in range(mv):
+                        if [rbt[0][0], rbt[0][1] - 1] in Obts:
+                            break
+
                         rbt[0][1] -= 1
 
                 elif rbt[1] == 'W':
                     for _ in range(mv):
+                        if [rbt[0][0] - 1, rbt[0][1]] in Obts:
+                            break
+
                         rbt[0][0] -= 1
 
                 print(f"\t\t{stp+1}. Move: {mv}")
