3fd7c4d24e9f4d52c65986a7171254f26d9b420f
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-04 16:18:43 +0200

    Daily 0874 v02.07 Driver update

--- Daily Question/0874. Walking Robot Simulation.py
+++ Daily Question/0874. Walking Robot Simulation.py
@@ -107,4 +107,7 @@ if __name__ == "__main__":
                 elif rbt[1] == 2:
                     rbt[0][0] += mv
 
+                elif rbt[1] == 3:
+                    rbt[0][1] -= mv
+
         print("\n")
