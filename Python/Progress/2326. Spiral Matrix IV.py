958cdb4e284abfb2873677664eab14c42d16424f
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-09 18:05:42 +0200

    LT 2326 v02.03 Comment

--- Python/Progress/2326. Spiral Matrix IV.py
+++ Python/Progress/2326. Spiral Matrix IV.py
@@ -52,6 +52,12 @@ https://leetcode.com/problems/spiral-matrix-iv/description/
 
 """
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
 
 if __name__ == "__main__":
 
