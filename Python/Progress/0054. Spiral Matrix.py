f2b5eba8ab7c776125a09806e9b23de39f627259
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-09 19:12:52 +0200

    LT 0054 01.02 Driver progress

--- Python/Progress/0054. Spiral Matrix.py
+++ Python/Progress/0054. Spiral Matrix.py
@@ -28,3 +28,12 @@ https://leetcode.com/problems/spiral-matrix/description/
         -100 <= matrix[i][j] <= 100
 
 """
+
+
+InputLst = [[[1,2,3],[4,5,6],[7,8,9]],
+            [[1,2,3,4],[5,6,7,8],[9,10,11,12]]]
+
+for case in InputLst:
+
+    print(case)
+    print()
