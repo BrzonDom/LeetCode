efef24fc9a580f534d451aedd45d82757346f559
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-09 19:29:51 +0200

    LT 0054 01.04 Document format

--- Python/Progress/0054. Spiral Matrix.py
+++ Python/Progress/0054. Spiral Matrix.py
@@ -9,15 +9,15 @@ https://leetcode.com/problems/spiral-matrix/description/
 
     Example 1:
 
-        Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+        Input: matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-        Output: [1,2,3,6,9,8,7,4,5]
+        Output: [1, 2, 3, 6, 9, 8, 7, 4, 5]
 
     Example 2:
 
-        Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+        Input: matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
 
-        Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+        Output: [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
 
 
     Constraints:
@@ -30,8 +30,8 @@ https://leetcode.com/problems/spiral-matrix/description/
 """
 
 
-InputLst = [[[1,2,3],[4,5,6],[7,8,9]],
-            [[1,2,3,4],[5,6,7,8],[9,10,11,12]]]
+InputLst = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]]
 
 for case in InputLst:
 
