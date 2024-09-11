fe58b32b5ae20b43dad0a4980f739a8c395a6fa0
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-11 20:50:24 +0200

    LT 2220 01.02 Document format

--- Python/Progress/2220. Minimum Bit Flips to Convert Number.py
+++ Python/Progress/2220. Minimum Bit Flips to Convert Number.py
@@ -20,11 +20,12 @@ https://leetcode.com/problems/minimum-bit-flips-to-convert-number/description/
 
         Output: 3
 
-        Explanation: The binary representation of 10 and 7 are 1010 and 0111 respectively. We can convert 10 to 7 in 3 steps:
-        - Flip the first bit from the right: 1010 -> 1011.
-        - Flip the third bit from the right: 1011 -> 1111.
-        - Flip the fourth bit from the right: 1111 -> 0111.
-        It can be shown we cannot convert 10 to 7 in less than 3 steps. Hence, we return 3.
+        Explanation: The binary representation of 10 and 7 are 1010 and 0111 respectively. We can convert 10 to 7 in
+            3 steps:
+            - Flip the first bit from the right: 1010 -> 1011.
+            - Flip the third bit from the right: 1011 -> 1111.
+            - Flip the fourth bit from the right: 1111 -> 0111.
+            It can be shown we cannot convert 10 to 7 in less than 3 steps. Hence, we return 3.
 
     Example 2:
 
@@ -32,11 +33,12 @@ https://leetcode.com/problems/minimum-bit-flips-to-convert-number/description/
 
         Output: 3
 
-        Explanation: The binary representation of 3 and 4 are 011 and 100 respectively. We can convert 3 to 4 in 3 steps:
-        - Flip the first bit from the right: 011 -> 010.
-        - Flip the second bit from the right: 010 -> 000.
-        - Flip the third bit from the right: 000 -> 100.
-        It can be shown we cannot convert 3 to 4 in less than 3 steps. Hence, we return 3.
+        Explanation: The binary representation of 3 and 4 are 011 and 100 respectively. We can convert 3 to 4 in
+            3 steps:
+            - Flip the first bit from the right: 011 -> 010.
+            - Flip the second bit from the right: 010 -> 000.
+            - Flip the third bit from the right: 000 -> 100.
+            It can be shown we cannot convert 3 to 4 in less than 3 steps. Hence, we return 3.
 
 
     Constraints:
