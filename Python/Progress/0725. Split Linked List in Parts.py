2150671dad722a31ffad31140b81255a714a0a33
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-13 00:47:54 +0200

    LT 0725 01.02 Document format

--- Python/Progress/0725. Split Linked List in Parts.py
+++ Python/Progress/0725. Split Linked List in Parts.py
@@ -18,17 +18,22 @@ https://leetcode.com/problems/split-linked-list-in-parts/description/
     Example 1:
 
         Input: head = [1,2,3], k = 5
+
         Output: [[1],[2],[3],[],[]]
+
         Explanation:
-        The first element output[0] has output[0].val = 1, output[0].next = null.
-        The last element output[4] is null, but its string representation as a ListNode is [].
+            The first element output[0] has output[0].val = 1, output[0].next = null.
+            The last element output[4] is null, but its string representation as a ListNode is [].
 
     Example 2:
 
         Input: head = [1,2,3,4,5,6,7,8,9,10], k = 3
+
         Output: [[1,2,3,4],[5,6,7],[8,9,10]]
+
         Explanation:
-        The input has been split into consecutive parts with size difference at most 1, and earlier parts are a larger size than the later parts.
+            The input has been split into consecutive parts with size difference at most 1, and earlier parts are a
+            larger size than the later parts.
 
 
     Constraints:
