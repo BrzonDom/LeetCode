7dd0eff2a76516928c2c2aa7428be16982ffb614
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-13 00:55:05 +0200

    LT 0725 01.10 Driver progress

--- Python/Progress/0725. Split Linked List in Parts.py
+++ Python/Progress/0725. Split Linked List in Parts.py
@@ -52,14 +52,16 @@ https://leetcode.com/problems/split-linked-list-in-parts/description/
 """
 
 
-InputLst = [[[1, 2, 3], 5],
-            [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3]]
+if __name__ == "__main__":
 
-for csCnt, case in enumerate(InputLst):
+    InputLst = [[[1, 2, 3], 5],
+                [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3]]
 
-    print(f"{csCnt + 1}.Case\n")
+    for csCnt, case in enumerate(InputLst):
 
-    print(f"\t{case[0]}")
-    print(f"\t{case[1]}")
+        print(f"{csCnt + 1}.Case\n")
 
-    print("\n")
+        print(f"\t{case[0]}")
+        print(f"\t{case[1]}")
+
+        print("\n")
