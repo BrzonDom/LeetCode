2c41448eb3a59bb4fa6f00828a8948ab8b809247
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-12 21:04:01 +0200

    LT 1684 01.04 Document format

--- Python/Progress/1684. Count the Number of Consistent Strings.py
+++ Python/Progress/1684. Count the Number of Consistent Strings.py
@@ -14,19 +14,19 @@ https://leetcode.com/problems/count-the-number-of-consistent-strings/description
 
     Example 1:
 
-        Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+        Input: allowed = "ab", words = ["ad", "bd", "aaab", "baa", "badab"]
         Output: 2
         Explanation: Strings "aaab" and "baa" are consistent since they only contain characters 'a' and 'b'.
 
     Example 2:
 
-        Input: allowed = "abc", words = ["a","b","c","ab","ac","bc","abc"]
+        Input: allowed = "abc", words = ["a", "b", "c", "ab", "ac", "bc", "abc"]
         Output: 7
         Explanation: All strings are consistent.
 
     Example 3:
 
-        Input: allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]
+        Input: allowed = "cad", words = ["cc", "acd", "b", "ba", "bac", "bad", "ac", "d"]
         Output: 4
         Explanation: Strings "cc", "acd", "ac", and "d" are consistent.
 
@@ -42,9 +42,9 @@ https://leetcode.com/problems/count-the-number-of-consistent-strings/description
 """
 
 
-InputLst = [["ab", ["ad","bd","aaab","baa","badab"]],
-            ["abc", ["a","b","c","ab","ac","bc","abc"]],
-            ["cad", ["cc","acd","b","ba","bac","bad","ac","d"]]]
+InputLst = [["ab", ["ad", "bd", "aaab", "baa", "badab"]],
+            ["abc", ["a", "b", "c", "ab", "ac", "bc", "abc"]],
+            ["cad", ["cc", "acd", "b", "ba", "bac", "bad", "ac", "d"]]]
 
 for case in InputLst:
 
