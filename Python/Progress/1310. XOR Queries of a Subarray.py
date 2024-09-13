44df4a81a44552e06d5f1a9937fdff56a40e3a64
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-13 14:34:39 +0200

    LT 1310 01.01 Document format

--- Python/Progress/1310. XOR Queries of a Subarray.py
+++ Python/Progress/1310. XOR Queries of a Subarray.py
@@ -4,17 +4,21 @@
 https://leetcode.com/problems/xor-queries-of-a-subarray/description/
 
 
-You are given an array arr of positive integers. You are also given the array queries where queries[i] = [lefti, righti].
+    You are given an array arr of positive integers. You are also given the array queries where
+    queries[i] = [left_i, right_i].
 
-For each query i compute the XOR of elements from lefti to righti (that is, arr[lefti] XOR arr[lefti + 1] XOR ... XOR arr[righti] ).
+    For each query i compute the XOR of elements from left_i to right_i
+    (that is, arr[left_i] XOR arr[left_i + 1] XOR ... XOR arr[right_i]).
 
-Return an array answer where answer[i] is the answer to the ith query.
+    Return an array answer where answer[i] is the answer to the i^th query.
 
 
     Example 1:
 
         Input: arr = [1,3,4,8], queries = [[0,1],[1,2],[0,3],[3,3]]
+
         Output: [2,7,14,8]
+
         Explanation:
         The binary representation of the elements in the array are:
         1 = 0001
@@ -30,14 +34,15 @@ Return an array answer where answer[i] is the answer to the ith query.
     Example 2:
 
         Input: arr = [4,8,2,10], queries = [[2,3],[1,3],[0,0],[0,3]]
+
         Output: [8,0,4,4]
 
 
     Constraints:
 
-        1 <= arr.length, queries.length <= 3 * 104
-        1 <= arr[i] <= 109
+        1 <= arr.length, queries.length <= 3 * 10^4
+        1 <= arr[i] <= 10^9
         queries[i].length == 2
-        0 <= lefti <= righti < arr.length
+        0 <= left_i <= right_i < arr.length
 
 """
