a443268db6cb57cf8367b3b0a80549435f27921a
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-15 05:23:40 +0200

    LT 1371 01.01 Document format

--- Python/Progress/1371. Find the Longest Substring Containing Vowels in Even Counts.py
+++ Python/Progress/1371. Find the Longest Substring Containing Vowels in Even Counts.py
@@ -4,31 +4,31 @@
 https://leetcode.com/problems/find-the-longest-substring-containing-vowels-in-even-counts/description/
 
 
-Given the string s, return the size of the longest substring containing each vowel an even number of times. That is, 'a', 'e', 'i', 'o', and 'u' must appear an even number of times.
+    Given the string s, return the size of the longest substring containing each vowel an even number of times. That is, 'a', 'e', 'i', 'o', and 'u' must appear an even number of times.
 
 
-    Example 1:
+        Example 1:
 
-        Input: s = "eleetminicoworoep"
-        Output: 13
-        Explanation: The longest substring is "leetminicowor" which contains two each of the vowels: e, i and o and zero of the vowels: a and u.
+            Input: s = "eleetminicoworoep"
+            Output: 13
+            Explanation: The longest substring is "leetminicowor" which contains two each of the vowels: e, i and o and zero of the vowels: a and u.
 
-    Example 2:
+        Example 2:
 
-        Input: s = "leetcodeisgreat"
-        Output: 5
-        Explanation: The longest substring is "leetc" which contains two e's.
+            Input: s = "leetcodeisgreat"
+            Output: 5
+            Explanation: The longest substring is "leetc" which contains two e's.
 
-    Example 3:
+        Example 3:
 
-        Input: s = "bcbcbc"
-        Output: 6
-        Explanation: In this case, the given string "bcbcbc" is the longest because all vowels: a, e, i, o and u appear zero times.
+            Input: s = "bcbcbc"
+            Output: 6
+            Explanation: In this case, the given string "bcbcbc" is the longest because all vowels: a, e, i, o and u appear zero times.
 
 
-    Constraints:
+        Constraints:
 
-        1 <= s.length <= 5 x 10^5
-        s contains only lowercase English letters.
+            1 <= s.length <= 5 x 10^5
+            s contains only lowercase English letters.
 
 """
