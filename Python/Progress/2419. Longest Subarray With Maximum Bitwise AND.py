013543a60029e24b43f7bcf853c1212b3831acfa
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-15 05:11:38 +0200

    LT 2419 01.02 Document format

--- Python/Progress/2419. Longest Subarray With Maximum Bitwise AND.py
+++ Python/Progress/2419. Longest Subarray With Maximum Bitwise AND.py
@@ -21,18 +21,22 @@ https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/descript
         Example 1:
 
             Input: nums = [1,2,3,3,2,2]
+
             Output: 2
+
             Explanation:
-            The maximum possible bitwise AND of a subarray is 3.
-            The longest subarray with that value is [3,3], so we return 2.
+                The maximum possible bitwise AND of a subarray is 3.
+                The longest subarray with that value is [3,3], so we return 2.
 
         Example 2:
 
             Input: nums = [1,2,3,4]
+
             Output: 1
+
             Explanation:
-            The maximum possible bitwise AND of a subarray is 4.
-            The longest subarray with that value is [4], so we return 1.
+                The maximum possible bitwise AND of a subarray is 4.
+                The longest subarray with that value is [4], so we return 1.
 
 
         Constraints:
