758014368754fc38a7dad8fce509a254d2df22ac
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-18 16:14:48 +0200

    LT 0884 03.18 1.Solution print function print progress

--- Python/Progress/0884. Uncommon Words from Two Sentences.py
+++ Python/Progress/0884. Uncommon Words from Two Sentences.py
@@ -94,9 +94,13 @@ def Slt01_WrdDctItr_Prt(Str1, Str2):
 
     print(f"\t1. String: {Str1}")
     print(f"\t2. String: {Str2}")
+    print()
 
     wrd1 = Str1.split()
     wrd2 = Str2.split()
+
+    print(f"\t1. Words: {wrd1}")
+    print(f"\t2. Words: {wrd2}")
 
 
 if __name__ == "__main__":
