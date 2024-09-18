cfb843a02de04dec42361e53ad6fa136f14b98b7
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-18 13:31:01 +0200

    LT 0884 03.04 Driver progress

--- Python/Progress/0884. Uncommon Words from Two Sentences.py
+++ Python/Progress/0884. Uncommon Words from Two Sentences.py
@@ -114,4 +114,7 @@ if __name__ == "__main__":
         print(f"\t1. Words: {wrd1}")
         print(f"\t2. Words: {wrd2}")
 
+        for wd in wrd1 + wrd2:
+            print(wd)
+
         print("\n")
