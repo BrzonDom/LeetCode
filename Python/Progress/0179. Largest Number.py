e49707f246292cfaae08e4043b866d4778f85968
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-20 12:23:01 +0200

    LT 0179 03.03 Driver update

--- Python/Progress/0179. Largest Number.py
+++ Python/Progress/0179. Largest Number.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
 
         print(f"\tNumbers: {Nmbs}")
 
-        for nm in Nmbs:
-            print(nm)
+        for nm in range(len(Nmbs)):
+            print(Nmbs[nm])
 
         print("\n")
