ce810e5e743a071fb06671c48b7de761a379c4ae
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-20 18:27:04 +0200

    LT 0214 01.02 Document format

--- Python/Progress/0214. Shortest Palindrome.py
+++ Python/Progress/0214. Shortest Palindrome.py
@@ -3,19 +3,24 @@
 
 https://leetcode.com/problems/shortest-palindrome/description/
 
+
     You are given a string s. You can convert s to a palindrome by adding characters in front of it.
 
     Return the shortest palindrome you can find by performing this transformation.
 
+
     Example 1:
 
         Input: s = "aacecaaa"
+
         Output: "aaacecaaa"
 
     Example 2:
 
         Input: s = "abcd"
+
         Output: "dcbabcd"
+
 
     Constraints:
 
