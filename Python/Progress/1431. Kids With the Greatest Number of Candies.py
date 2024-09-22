6d99c699b3b1313ce2cad9c32f82f0fb5aa0c5da
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-22 20:43:15 +0200

    LT 1431 01.04 Document format

--- Python/Progress/1431. Kids With the Greatest Number of Candies.py
+++ Python/Progress/1431. Kids With the Greatest Number of Candies.py
@@ -20,11 +20,11 @@ https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/descripti
         Output: [true,true,true,false,true]
 
         Explanation: If you give all extraCandies to:
-        - Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
-        - Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
-        - Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
-        - Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
-        - Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+            - Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+            - Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+            - Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+            - Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+            - Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
 
     Example 2:
 
@@ -33,7 +33,7 @@ https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/descripti
         Output: [true,false,false,false,false]
 
         Explanation: There is only 1 extra candy.
-        Kid 1 will always have the greatest number of candies, even if a different kid is given the extra candy.
+            Kid 1 will always have the greatest number of candies, even if a different kid is given the extra candy.
 
     Example 3:
 
