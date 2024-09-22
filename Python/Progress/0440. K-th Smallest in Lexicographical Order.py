068754d1bdc592a0ca032a94a52dbb701e6cf6d7
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-22 22:43:05 +0200

    LT 0440 01.10 Driver progress

--- Python/Progress/0440. K-th Smallest in Lexicographical Order.py
+++ Python/Progress/0440. K-th Smallest in Lexicographical Order.py
@@ -40,14 +40,16 @@ https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/description
 """
 
 
-InputLst = [[13, 2],
-            [1, 1]]
+if __name__ == "__main__":
 
-for csCnt, case in enumerate(InputLst):
+    InputLst = [[13, 2],
+                [1, 1]]
 
-    print(f"{csCnt + 1}.Case\n")
+    for csCnt, case in enumerate(InputLst):
 
-    print(f"\t{case[0]}")
-    print(f"\t{case[1]}")
+        print(f"{csCnt + 1}.Case\n")
 
-    print("\n")
+        print(f"\t{case[0]}")
+        print(f"\t{case[1]}")
+
+        print("\n")
