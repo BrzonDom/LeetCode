d7063dfde559c3ab52dfa996157e34e4fc1ade0d
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-26 13:15:06 +0200

    LT 0729 01.03 Document format

--- Python/Progress/0729. My Calendar I.py
+++ Python/Progress/0729. My Calendar I.py
@@ -12,29 +12,29 @@ https://leetcode.com/problems/my-calendar-i/description/
 
     Implement the MyCalendar class:
 
-    MyCalendar() Initializes the calendar object.
-    boolean book(int start, int end) Returns true if the event can be added to the calendar successfully without causing a double booking. Otherwise, return false and do not add the event to the calendar.
+        MyCalendar() Initializes the calendar object.
+        boolean book(int start, int end) Returns true if the event can be added to the calendar successfully without causing a double booking. Otherwise, return false and do not add the event to the calendar.
 
 
     Example 1:
 
-        Input
-        ["MyCalendar", "book", "book", "book"]
-        [[], [10, 20], [15, 25], [20, 30]]
+        Input:
+            ["MyCalendar", "book", "book", "book"]
+            [[], [10, 20], [15, 25], [20, 30]]
 
-        Output
-        [null, true, false, true]
+        Output:
+            [null, true, false, true]
 
-        Explanation
-        MyCalendar myCalendar = new MyCalendar();
-        myCalendar.book(10, 20); // return True
-        myCalendar.book(15, 25); // return False, It can not be booked because time 15 is already booked by another event.
-        myCalendar.book(20, 30); // return True, The event can be booked, as the first event takes every time less than 20, but not including 20.
+        Explanation:
+            MyCalendar myCalendar = new MyCalendar();
+            myCalendar.book(10, 20); // return True
+            myCalendar.book(15, 25); // return False, It can not be booked because time 15 is already booked by another event.
+            myCalendar.book(20, 30); // return True, The event can be booked, as the first event takes every time less than 20, but not including 20.
 
 
     Constraints:
 
-    0 <= start < end <= 109
-    At most 1000 calls will be made to book.
+        0 <= start < end <= 109
+        At most 1000 calls will be made to book.
 
 """
