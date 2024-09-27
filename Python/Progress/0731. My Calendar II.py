61a0de4e467dfa6fc2de15e8b632c8f0dd04bbf3
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-27 09:17:57 +0200

    LT 0731 01.04 Document format

--- Python/Progress/0731. My Calendar II.py
+++ Python/Progress/0731. My Calendar II.py
@@ -22,30 +22,30 @@ https://leetcode.com/problems/my-calendar-ii/description/
 
     Example 1:
 
-        Input
+        Input:
             ["MyCalendarTwo", "book", "book", "book", "book", "book", "book"]
             [[], [10, 20], [50, 60], [10, 40], [5, 15], [5, 10], [25, 55]]
 
-        Output
+        Output:
             [null, true, true, true, false, true, true]
 
-        Explanation
-            MyCalendarTwo myCalendarTwo = new MyCalendarTwo();
-            myCalendarTwo.book(10, 20); // return True, The event can be booked.
-            myCalendarTwo.book(50, 60); // return True, The event can be booked.
-            myCalendarTwo.book(10, 40); // return True, The event can be double booked.
-            myCalendarTwo.book(5, 15);  // return False, The event cannot be booked, because it would result in a
-            triple booking.
-            myCalendarTwo.book(5, 10); // return True, The event can be booked, as it does not use time 10 which is
-            already double booked.
-            myCalendarTwo.book(25, 55); // return True, The event can be booked, as the time in [25, 40) will be
-            double booked with the third event, the time [40, 50) will be single booked, and the time [50, 55)
-            will be double booked with the second event.
+        Explanation:
+            - MyCalendarTwo myCalendarTwo = new MyCalendarTwo();
+            - myCalendarTwo.book(10, 20); // return True, The event can be booked.
+            - myCalendarTwo.book(50, 60); // return True, The event can be booked.
+            - myCalendarTwo.book(10, 40); // return True, The event can be double booked.
+            - myCalendarTwo.book(5, 15);  // return False, The event cannot be booked, because it would result in a
+              triple booking.
+            - myCalendarTwo.book(5, 10); // return True, The event can be booked, as it does not use time 10 which is
+              already double booked.
+            - myCalendarTwo.book(25, 55); // return True, The event can be booked, as the time in [25, 40) will be
+              double booked with the third event, the time [40, 50) will be single booked, and the time [50, 55)
+              will be double booked with the second event.
 
 
     Constraints:
 
-        0 <= start < end <= 109
+        0 <= start < end <= 10^9
         At most 1000 calls will be made to book.
 
 """
