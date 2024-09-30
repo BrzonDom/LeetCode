731f3b653b55ab6bfba1d30ac0c5a54363068746
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-09-30 19:37:34 +0200

    LT 0641 01.04 Document format

--- Python/Progress/0641. Design Circular Deque.py
+++ Python/Progress/0641. Design Circular Deque.py
@@ -8,27 +8,27 @@ https://leetcode.com/problems/design-circular-deque/description/
 
     Implement the MyCircularDeque class:
 
-        MyCircularDeque(int k) Initializes the deque with a maximum size of k.
-        boolean insertFront() Adds an item at the front of Deque. Returns true if the operation is successful, or false otherwise.
-        boolean insertLast() Adds an item at the rear of Deque. Returns true if the operation is successful, or false otherwise.
-        boolean deleteFront() Deletes an item from the front of Deque. Returns true if the operation is successful, or false otherwise.
-        boolean deleteLast() Deletes an item from the rear of Deque. Returns true if the operation is successful, or false otherwise.
-        int getFront() Returns the front item from the Deque. Returns -1 if the deque is empty.
-        int getRear() Returns the last item from Deque. Returns -1 if the deque is empty.
-        boolean isEmpty() Returns true if the deque is empty, or false otherwise.
-        boolean isFull() Returns true if the deque is full, or false otherwise.
+        - MyCircularDeque(int k) Initializes the deque with a maximum size of k.
+        - boolean insertFront() Adds an item at the front of Deque. Returns true if the operation is successful, or false otherwise.
+        - boolean insertLast() Adds an item at the rear of Deque. Returns true if the operation is successful, or false otherwise.
+        - boolean deleteFront() Deletes an item from the front of Deque. Returns true if the operation is successful, or false otherwise.
+        - boolean deleteLast() Deletes an item from the rear of Deque. Returns true if the operation is successful, or false otherwise.
+        - int getFront() Returns the front item from the Deque. Returns -1 if the deque is empty.
+        - int getRear() Returns the last item from Deque. Returns -1 if the deque is empty.
+        - boolean isEmpty() Returns true if the deque is empty, or false otherwise.
+        - boolean isFull() Returns true if the deque is full, or false otherwise.
 
 
     Example 1:
 
-        Input
+        Input:
             ["MyCircularDeque", "insertLast", "insertLast", "insertFront", "insertFront", "getRear", "isFull", "deleteLast", "insertFront", "getFront"]
             [[3], [1], [2], [3], [4], [], [], [], [4], []]
 
-        Output
+        Output:
             [null, true, true, true, false, 2, true, true, true, 4]
 
-        Explanation
+        Explanation:
             MyCircularDeque myCircularDeque = new MyCircularDeque(3);
             myCircularDeque.insertLast(1);  // return True
             myCircularDeque.insertLast(2);  // return True
