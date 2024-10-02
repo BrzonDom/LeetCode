b62e300a1f21a51862451099b20939b2e87e0904
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-02 19:26:22 +0200

    LT 0345 01.01 Document format

--- Python/Progress/0345. Reverse Vowels of a String.py
+++ Python/Progress/0345. Reverse Vowels of a String.py
@@ -3,29 +3,31 @@
 
 https://leetcode.com/problems/reverse-vowels-of-a-string/description/
 
-Given a string s, reverse only all the vowels in the string and return it.
 
-The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+    Given a string s, reverse only all the vowels in the string and return it.
 
-Example 1:
+    The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
 
-    Input: s = "IceCreAm"
 
-    Output: "AceCreIm"
+    Example 1:
 
-    Explanation:
+        Input: s = "IceCreAm"
 
-    The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+        Output: "AceCreIm"
 
-Example 2:
+        Explanation:
+            The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
 
-    Input: s = "leetcode"
+    Example 2:
 
-    Output: "leotcede"
+        Input: s = "leetcode"
 
-Constraints:
+        Output: "leotcede"
 
-    1 <= s.length <= 3 * 105
-    s consist of printable ASCII characters.
+
+    Constraints:
+
+        1 <= s.length <= 3 * 10^5
+        s consist of printable ASCII characters.
 
 """
