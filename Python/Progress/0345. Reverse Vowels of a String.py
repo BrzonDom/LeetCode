a8d50c81df299b44b3e104128cfeda17de14fbcc
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-03 19:08:35 +0200

    LT 0345 03.10 Driver progress

--- Python/Progress/0345. Reverse Vowels of a String.py
+++ Python/Progress/0345. Reverse Vowels of a String.py
@@ -133,18 +133,7 @@ if __name__ == "__main__":
 
         while lft < rgt:
 
-            print(srt_lst[lft])
-
             lft += 1
-        print()
-
-        lft = 0
-        rgt = ln_srt - 1
-
-        while lft < rgt:
-
-            print(srt_lst[rgt])
-
             rgt -= 1
 
         print("\n")
