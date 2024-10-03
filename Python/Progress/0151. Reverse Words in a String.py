38ae7f99cb3dac8e2d51f5e8bb79ce07864888c6
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-03 19:37:21 +0200

    LT 0151 01.02 Document format

--- Python/Progress/0151. Reverse Words in a String.py
+++ Python/Progress/0151. Reverse Words in a String.py
@@ -10,7 +10,8 @@ https://leetcode.com/problems/reverse-words-in-a-string/description/
 
     Return a string of the words in reverse order concatenated by a single space.
 
-    Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+    Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string
+    should only have a single space separating the words. Do not include any extra spaces.
 
 
     Example 1:
