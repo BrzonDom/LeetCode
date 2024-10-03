bc387edba2f6c05381fd3c9318069d5bef279455
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-03 19:36:48 +0200

    LT 0151 01.01 Document format

--- Python/Progress/0151. Reverse Words in a String.py
+++ Python/Progress/0151. Reverse Words in a String.py
@@ -3,35 +3,43 @@
 
 https://leetcode.com/problems/reverse-words-in-a-string/description/
 
-Given an input string s, reverse the order of the words.
 
-A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+    Given an input string s, reverse the order of the words.
 
-Return a string of the words in reverse order concatenated by a single space.
+    A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
 
-Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+    Return a string of the words in reverse order concatenated by a single space.
 
-Example 1:
+    Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
 
-    Input: s = "the sky is blue"
-    Output: "blue is sky the"
 
-Example 2:
+    Example 1:
 
-    Input: s = "  hello world  "
-    Output: "world hello"
-    Explanation: Your reversed string should not contain leading or trailing spaces.
+        Input: s = "the sky is blue"
 
-Example 3:
+        Output: "blue is sky the"
 
-    Input: s = "a good   example"
-    Output: "example good a"
-    Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+    Example 2:
 
-Constraints:
+        Input: s = "  hello world  "
 
-    1 <= s.length <= 104
-    s contains English letters (upper-case and lower-case), digits, and spaces ' '.
-    There is at least one word in s
+        Output: "world hello"
+
+        Explanation: Your reversed string should not contain leading or trailing spaces.
+
+    Example 3:
+
+        Input: s = "a good   example"
+
+        Output: "example good a"
+
+        Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+
+    Constraints:
+
+        1 <= s.length <= 10^4
+        s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+        There is at least one word in s
 
 """
