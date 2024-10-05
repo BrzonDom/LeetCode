b26169dedcba1f51a10521373ed7566467b61177
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-05 23:53:08 +0200

    LT 0283 01.02 Document format

--- Python/Progress/0283. Move Zeroes.py
+++ Python/Progress/0283. Move Zeroes.py
@@ -4,7 +4,8 @@
 https://leetcode.com/problems/move-zeroes/description/
 
 
-    Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+    Given an integer array nums, move all 0's to the end of it while maintaining the relative order of
+    the non-zero elements.
 
     Note that you must do this in-place without making a copy of the array.
 
@@ -12,17 +13,19 @@ https://leetcode.com/problems/move-zeroes/description/
     Example 1:
 
         Input: nums = [0,1,0,3,12]
+
         Output: [1,3,12,0,0]
 
     Example 2:
 
         Input: nums = [0]
+
         Output: [0]
 
 
     Constraints:
 
-        1 <= nums.length <= 104
-        -231 <= nums[i] <= 231 - 1
+        1 <= nums.length <= 10^4
+        -2^31 <= nums[i] <= 2^31 - 1
 
 """
