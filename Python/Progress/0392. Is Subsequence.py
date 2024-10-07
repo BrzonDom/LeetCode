ee3f1b9baa9fe7be813313c44eb3b9beae77f500
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-07 23:54:43 +0200

    LT 0392 01.01 Document format

--- Python/Progress/0392. Is Subsequence.py
+++ Python/Progress/0392. Is Subsequence.py
@@ -3,24 +3,29 @@
 
 https://leetcode.com/problems/is-subsequence/description/
 
-Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 
-A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+    Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 
-Example 1:
+    A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
-    Input: s = "abc", t = "ahbgdc"
-    Output: true
 
-Example 2:
+    Example 1:
 
-    Input: s = "axc", t = "ahbgdc"
-    Output: false
+        Input: s = "abc", t = "ahbgdc"
 
-Constraints:
+        Output: true
 
-    0 <= s.length <= 100
-    0 <= t.length <= 104
-    s and t consist only of lowercase English letters.
+    Example 2:
+
+        Input: s = "axc", t = "ahbgdc"
+
+        Output: false
+
+
+    Constraints:
+
+        0 <= s.length <= 100
+        0 <= t.length <= 104
+        s and t consist only of lowercase English letters.
 
 """
