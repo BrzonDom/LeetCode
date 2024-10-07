c84475aff986bf81806b7367c83fe239533812ab
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-07 23:58:06 +0200

    LT 0392 01.02 Document format

--- Python/Progress/0392. Is Subsequence.py
+++ Python/Progress/0392. Is Subsequence.py
@@ -6,7 +6,9 @@ https://leetcode.com/problems/is-subsequence/description/
 
     Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 
-    A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+    A subsequence of a string is a new string that is formed from the original string by deleting some (can be none)
+    of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a
+    subsequence of "abcde" while "aec" is not).
 
 
     Example 1:
@@ -25,7 +27,7 @@ https://leetcode.com/problems/is-subsequence/description/
     Constraints:
 
         0 <= s.length <= 100
-        0 <= t.length <= 104
+        0 <= t.length <= 10^4
         s and t consist only of lowercase English letters.
 
 """
