0a2026a4b5be1f75c5c23dd282d40e87be1c0f57
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-19 12:11:52 +0200

    LT 0392 02.09 Driver print upgrade

--- Python/Progress/0392. Is Subsequence.py
+++ Python/Progress/0392. Is Subsequence.py
@@ -77,6 +77,13 @@ if __name__ == "__main__":
                 print(f"\t\t\tSrt[{idx+1}] = Sbs[{cnt_sbs-1}] = {crc}")
 
                 if cnt_sbs == lng_sbs:
+                    print()
+                    print("\tSubset is a subset of the string")
+
                     break
+
+            if cnt_sbs != lng_sbs:
+                print()
+                print("\tSubset is not a subset of the string")
 
         print("\n")
