c0ddf840081a16ebe3c4aca696d35a1868fcc2d4
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-10-19 12:33:39 +0200

    LT 0432 01.05 Fix

--- Python/Progress/0432. All O`one Data Structure.py
+++ Python/Progress/0432. All O`one Data Structure.py
@@ -65,14 +65,20 @@ Constraints:
 class AllOne:
 
     def __init__(self):
+        pass
 
     def inc(self, key: str) -> None:
+        pass
 
     def dec(self, key: str) -> None:
+        pass
 
     def getMaxKey(self) -> str:
+        pass
 
     def getMinKey(self) -> str:
+        pass
+
 
 # Your AllOne object will be instantiated and called as such:
 # obj = AllOne()
