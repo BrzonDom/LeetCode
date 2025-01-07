d002bfacb1e8c24035e0ff3a13ad45b880ee2cb3
Dominik Brzoň <brzon.l.dominik@gmail.com>
2025-01-07 23:59:20 +0100

    LT 0643 03.07 Driver progress

--- Python/Progress/0643. Maximum Average Subarray I.py
+++ Python/Progress/0643. Maximum Average Subarray I.py
@@ -109,8 +109,8 @@ if __name__ == "__main__":
         print(f"\t\tSum: {sum_cur}")
 
         for i in range(1, len(Nmbs) - Rng + 1):
+            sum_cur += -Nmbs[i-1] + Nmbs[Rng+i-1]
             nmbs_cur = nmbs_cur[1:] + [Nmbs[Rng+i-1]]
-            sum_cur = sum(nmbs_cur)
 
             print(f"\t{nmbs_cur}")
             print(f"\t\tSum: {sum_cur}")
