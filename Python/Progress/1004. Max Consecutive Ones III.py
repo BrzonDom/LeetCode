718a3b2c0e223c690ec1c90eb9aad8229f5f99ee
Dominik Brzoň <brzon.l.dominik@gmail.com>
2025-01-13 23:39:21 +0100

    LT 1004 01.01 Document format

--- Python/Progress/1004. Max Consecutive Ones III.py
+++ Python/Progress/1004. Max Consecutive Ones III.py
@@ -3,28 +3,33 @@
 
 https://leetcode.com/problems/max-consecutive-ones-iii/description/
 
-Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+    Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
 
 
-Example 1:
+    Example 1:
 
-    Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
-    Output: 6
-    Explanation: [1,1,1,0,0,1,1,1,1,1,1]
-    Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+        Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
 
-Example 2:
+        Output: 6
 
-    Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
-    Output: 10
-    Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
-    Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+        Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+            Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+    Example 2:
+
+        Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+
+        Output: 10
+
+        Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+            Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
 
 
-Constraints:
+    Constraints:
 
-    1 <= nums.length <= 105
-    nums[i] is either 0 or 1.
-    0 <= k <= nums.length
+        1 <= nums.length <= 105
+        nums[i] is either 0 or 1.
+        0 <= k <= nums.length
 
 """
