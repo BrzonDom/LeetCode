83c473c953a84b3a311a0ceec7c1338ad36b90b4
Dominik Brzoň <brzon.l.dominik@gmail.com>
2025-05-10 23:56:54 +0200

    LT 1732 01.05 Document format

--- Python/Progress/1732. Find the Highest Altitude.py
+++ Python/Progress/1732. Find the Highest Altitude.py
@@ -13,19 +13,21 @@ https://leetcode.com/problems/find-the-highest-altitude/description/
 
     Example 1:
 
-        Input: gain = [-5,1,5,0,-7]
+        Input: gain = [-5, 1, 5, 0, -7]
 
         Output: 1
 
-        Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+        Explanation: The altitudes are [0, -5, -4, 1, 1, -6].
+            The highest is 1.
 
     Example 2:
 
-        Input: gain = [-4,-3,-2,-1,4,3,2]
+        Input: gain = [-4, -3, -2, -1, 4, 3, 2]
 
         Output: 0
 
-        Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
+        Explanation: The altitudes are [0, -4, -7, -9, -10, -6, -3, -1].
+            The highest is 0.
 
 
     Constraints:
@@ -48,8 +50,8 @@ https://leetcode.com/problems/find-the-highest-altitude/description/
 """
 
 
-Input_Lst = [[-5,1,5,0,-7],
-             [-4,-3,-2,-1,4,3,2]]
+Input_Lst = [[-5, 1, 5, 0, -7],
+             [-4, -3, -2, -1, 4, 3, 2]]
 
 for case in Input_Lst:
 
