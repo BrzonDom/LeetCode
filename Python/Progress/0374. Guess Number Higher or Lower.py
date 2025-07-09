74cfe5a741f77ae41e50278a85d11d0bad65bffb
Dominik Brzoň <brzon.l.dominik@gmail.com>
2025-07-09 17:21:36 +0200

    LT 0374 01.03 Comment

--- Python/Progress/0374. Guess Number Higher or Lower.py
+++ Python/Progress/0374. Guess Number Higher or Lower.py
@@ -54,3 +54,11 @@ https://leetcode.com/problems/guess-number-higher-or-lower/description/
         LeetCode 75
 
 """
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
